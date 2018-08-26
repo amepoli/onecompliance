cd7717fbebd57ac3982cab4602b195734a12848a
Nicola Capovilla <nicolacapovilla@iMac-di-Nicola.local>
2018-08-26 23:19:07 +0200

    Modifications to toolbar and navbar

---
+++ src/app/navigation/i18n/it.ts
@@ -0,0 +1,30 @@
+export const locale = {
+    lang: 'it',
+    data: {
+        'NAV': {
+            'APPLICATIONS': 'Applicazioni',
+            'ORGANIZATION': 'Organizzazione',
+            'EMPLOYEES'   : 'Dipendenti',
+            'MGTUNITS'    : 'Centri Gestionali',
+            'PROCESSES'   : 'Processi',
+            'RISKS'       : 'Rischi',
+            'DASHBOARDS'  : 'Dashboards',
+            'CALENDAR'    : 'Calendario',
+            'ECOMMERCE'   : 'E-Commerce',
+            'ACADEMY'     : 'Academy',
+            'MAIL'        : {
+                'TITLE': 'Mail',
+                'BADGE': '25'
+            },
+            'MAIL_NGRX'        : {
+                'TITLE': 'Mail Ngrx',
+                'BADGE': '13'
+            },
+            'CHAT'        : 'Chat',
+            'FILE_MANAGER': 'File Manager',
+            'CONTACTS'    : 'Contatti',
+            'TODO'        : 'To-Do',
+            'SCRUMBOARD'  : 'Scrumboard'
+        }
+    }
+};
