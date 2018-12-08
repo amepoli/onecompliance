bb92149ef8c78b50ac1e652999db2491d3e28d16
Nicola Capovilla <nicolacapovilla@iMac-di-Nicola.local>
2018-12-08 23:43:11 +0100

    Change lambda

--- lambda/centri_gestionali/index.js
+++ lambda/centri_gestionali/index.js
@@ -3,12 +3,10 @@ var pg = require("pg");
 
 exports.handler = function(event, context, callback) {
  
-console.log(event.queryStringParameters);
- 
 var conn = "postgres://postgres:et2themax@goricotest.caxbbckt9xen.eu-central-1.rds.amazonaws.com/GoRiCo";
 var client = new pg.Client(conn);
 var codice_part = event.queryStringParameters.codice_part;
-console.log(`codice part is: ${codice_part}`);
+var id = event.queryStringParameters.id;
 
 var queryString = 
 `SELECT centri_1.id_centro_gest AS ID, centri_1.codice, centri_1.descrizione, 
@@ -28,27 +26,125 @@ ON id_responsabile=id_anagrafica
 WHERE centri_1.codice_part='${codice_part}' AND anagr.codice_part='${codice_part}'
 ORDER BY ID;`;
 
+var queryString_new =
+`SELECT cg.id_centro_gest, CG.codice, CG.descrizione, 
+entrasp.anagrafiche_id_codcognnome('${codice_part}',cg.id_responsabile) as responsabile,
+cg.ute_ref,
+entrasp.centri_gestionali_descr('${codice_part}',cg.id_centro_gest_parent) as parente
+FROM entrasp.centri_gestionali CG WHERE cg.codice_part='${codice_part}';`
+
+var form = [
+    {
+      type: 'input',
+      label: 'Codice',
+      inputType: 'text',
+      name: 'code',
+      value: 'CdA',
+      validations: [
+        {
+          name: 'required',
+          validator: 'Validators.required',
+          message: 'Codice mancante'
+        },
+        {
+          name: 'pattern',
+          validator: '^[a-zA-Z]+$',
+          message: 'Accetta solo testo senza spazi'
+        }
+      ]
+    },
+    {
+        type: 'input',
+        label: 'Descrizione',
+        inputType: 'text',
+        name: 'Description',
+        value: '',
+        validations: [
+          {
+            name: 'required',
+            validator: 'Validators.required',
+            message: 'Descrizione mancante'
+          },
+        ]
+      },
+      {
+        type: 'combobox',
+        label: 'Centro gestionale di livello superiore',
+        name: 'superiore',
+        selected: '3',
+        options: [{id: '1', name: 'Amministrazione'}, 
+                  {id: '2', name: 'Commerciale'},
+                  {id: '3', name: 'Compliance'}, 
+                  {id: '4', name: 'Consiglio di Amministrazione'}]
+      },
+      {
+        type: 'combobox',
+        label: 'Responsabile',
+        name: 'responsabile',
+        selected: '2',
+        options: [{id: '1', name: 'Poli Amedeo'}, 
+                  {id: '2', name: 'Nicola Capovilla'},
+                  {id: '3', name: 'Francesco Guarneri'}, 
+                  {id: '4', name: 'Arlotta Carlo'}]
+      },
+      {
+        type: 'combobox',
+        label: 'Utente Referente',
+        name: 'referente',
+        selected: '4',
+        options: [{id: '1', name: 'Poli Amedeo'}, 
+                  {id: '2', name: 'Nicola Capovilla'},
+                  {id: '3', name: 'Francesco Guarneri'}, 
+                  {id: '4', name: 'Arlotta Carlo'}]
+      },
+      {
+        type: 'checkbox',
+        label: 'Supervisore di tutti i sondaggi',
+        name: 'check_supervisore',
+        value: false
+      },
+      {
+        type: 'checkbox',
+        label: 'Gestore di tutti i modelli di test',
+        name: 'check_gestore',
+        value: false
+      }
+    ];
+
 client.connect();
-console.log('Connected to PostgreSQL database');
+//console.log('Connected to PostgreSQL database');
  
 //var id = event.id;
-var query = client.query(queryString);
-query.on("row", function (row, result) {
- result.addRow(row);
-});
-query.on("end", function (result) {
- var jsonString = JSON.stringify(result.rows);
- var jsonObj = JSON.parse(jsonString);
-// console.log(jsonString);
- client.end();
+
+if (id === '') {
+  var query = client.query(queryString_new);
+  query.on("row", function (row, result) {
+  result.addRow(row);
+  });
+  query.on("end", function (result) {
+    var jsonString = JSON.stringify(result.rows);
+    var jsonObj = JSON.parse(jsonString);
+    // console.log(jsonString);
+   client.end();
 // context.succeed(jsonObj);
- var response = {
+   var response = {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
         "body": JSON.stringify(jsonObj),
         "isBase64Encoded": false
     };
- callback(null, response);
-});
-};
+    callback(null, response);
+ });
+} else {    //query one element
+  var jsonObj = JSON.stringify(form);
+  client.end();
+  var response = {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+        "body": jsonObj,
+        "isBase64Encoded": false
+    };
+    callback(null, response);
+}
 
+};
