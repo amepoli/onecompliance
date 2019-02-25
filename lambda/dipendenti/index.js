94a12b01b3e28f543fe1cde7bba661d8a1e1db05
Nicola CAPOVILLA <nicola.capovilla@st.com>
2019-02-25 19:07:35 +0100

    Fix lambda Dipendenti

--- lambda/dipendenti/index.js
+++ lambda/dipendenti/index.js
@@ -1,12 +1,11 @@
 var processor = require('process_request');
 
-
 exports.handler = function(event, context, callback) {
 
 var codice_part = event.queryStringParameters.key1;
 var id = event.queryStringParameters.key2;
 
-var form = [
+var element_form = [
     { 
       type: 'input',
       label: 'ID',
@@ -79,6 +78,29 @@ var form = [
     }
     ];
 
+var search_form = [
+    {type: 'input',
+		 label: 'Nome',
+		 inputType: 'text',
+		 name: 'nome',
+		 value: '',
+		 readonly: 'false'
+		  },
+	  {type: 'input',
+		 label: 'Cognome',
+		 inputType: 'text',
+		 name: 'cognome',
+		 value: '',
+		 readonly: 'false'
+		}	
+    ];
+
+var form = {
+    element_form: element_form,
+    search_form: search_form
+};
+
+
 var queries = {};
 
 queries.list = 
@@ -108,24 +130,38 @@ queries.next =
 queries.delete = `DELETE FROM entrasp.anagrafiche_id
       WHERE codice_part='${codice_part}' AND id_anagrafica='${id}';`;
 
+queries.conditions = {
+  nome: {condition: 'nome Like \'%$param%\''},
+  cognome: {condition: 'cognome Like \'%$param%\''}
+};
+
 var body;
 
-if (event.httpMethod === "POST" || event.httpMethod === "PUT") {
+if ((event.httpMethod === "POST" && event.queryStringParameters.operation != 'search') || event.httpMethod === "PUT") {
    body = JSON.parse(event.body.toString()); // drove me crazy!!!!
-   
+
+  // Check of non null value needed for all combo box selected choices
+  // Please note the difference between processing numeric or string types of selected choices
+  var codice = body.codice ? `${body.codice}` : null;
+  if (codice) codice= '\'' + codice.replace(/'/g, "''") + '\''; //add external quotes and replace internal single quotes with two quotes (Postgres syntax)
+  var id_anagrafica = body.id_anagrafica ? `${body.id_anagrafica}` : null;
+  var cognome = body.cognome ? `${body.cognome}` : null;
+  if (cognome) cognome= '\'' + cognome.replace(/'/g, "''") + '\''; //add external quotes and replace internal single quotes with two quotes (Postgres syntax)
+  var nome = body.nome ? `${body.nome}` : null;
+  if (nome) nome= '\'' + nome.replace(/'/g, "''") + '\''; //add external quotes and replace internal single quotes with two quotes (Postgres syntax)
+
    queries.new = `INSERT INTO entrasp.anagrafiche_id 
         (codice_part, id_anagrafica, codice, cognome, nome)
         VALUES
-       ('${codice_part}', ${body['id_anagrafica']}, '${body['codice']}', '${body['cognome']}', 
-         '${body['nome']}')
+       ('${codice_part}', ${id_anagrafica}, ${codice}, ${cognome}, ${nome})
          RETURNING id_anagrafica;`;
     
    queries.update = `
          UPDATE entrasp.anagrafiche_id
-         SET codice = '${body['codice']}',
-             cognome = '${body['cognome']}',
-             nome = '${body['nome']}'
-         WHERE codice_part='${codice_part}' AND id_anagrafica='${body['id_anagrafica']}'`;   
+         SET codice = ${codice},
+             cognome = ${cognome},
+             nome = ${nome}
+         WHERE codice_part='${codice_part}' AND id_anagrafica=${id_anagrafica}`;   
 }
 
 var ret_callback = function(return_value) {
