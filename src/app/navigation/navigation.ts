ef09296f56da14a4706fa6fe464fce62b1804e1a
nikapov <nicola.capovilla@gmail.com>
2019-08-27 20:06:51 +0200

    Procedure aziendali

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -35,7 +35,7 @@ export const navigation: FuseNavigation[] = [
                 translate: 'NAV.PROCESSES',
                 icon: 'build',
                 type : 'item',
-                url  : '/login'
+                url  : '/gorico/main-table/procedure_aziendali'
             },
             {
                 id       : 'workflow',
