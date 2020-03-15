70500fc7c7ad89c88c4eed84657c8ada3526b42e
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-03-15 17:41:28 +0500

    Added sweetalert2 patch

--- patch.js
+++ patch.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
 
-fs.readFile(f, 'utf8', function (err,data) {
+fs.readFile(f, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
@@ -12,4 +12,23 @@ fs.readFile(f, 'utf8', function (err,data) {
   });
 
   console.log('Crypto patch applied');
+});
+
+
+// ----------------------------------------------
+// SweetAlert2 fix
+
+const sweetalert2File = 'node_modules/sweetalert2/sweetalert2.d.ts';
+
+fs.readFile(sweetalert2File, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = data.replace('[string?, string?, SweetAlertIcon?]', '[string, string, SweetAlertIcon]');
+
+  fs.writeFile(sweetalert2File, result, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+
+  console.log('Sweetalert2 patch applied');
 });
