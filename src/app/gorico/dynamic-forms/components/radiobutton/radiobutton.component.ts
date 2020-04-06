3d044cc8f4ec8acf9d77f1e9f1c7e03801ffbb7b
nikapov <nicola.capovilla@gmail.com>
2020-04-06 21:24:45 +0200

    Review of questionnaire row events.

--- src/app/gorico/dynamic-forms/components/radiobutton/radiobutton.component.ts
+++ src/app/gorico/dynamic-forms/components/radiobutton/radiobutton.component.ts
@@ -23,8 +23,11 @@ export class RadiobuttonComponent implements OnInit {
 
   constructor(private pubsubService: NgxPubSubService) { }
   ngOnInit() {
-    console.log(this.field);
-    this.chosenItem = this.field.options.find(o => o.id === this.field.value);
+    const _this = this;
+    console.log(_this.field);
+    _this.chosenItem = _this.field.options.find(o => o.id === _this.field.value);
+    // trigger an event the first time
+    setTimeout(() => { _this.pubsubService.publishEvent(_this.field.eventName, { origin: _this.field.name, index: _this.field.index, valueSet: _this.field.fullValueSet, data: _this.field.value, type: 'radiobutton' }); }, 50);
   }
 
   onCheck(event: any): void {
