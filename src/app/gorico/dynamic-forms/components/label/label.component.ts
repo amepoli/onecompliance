f7fc1138f2b4435d49604fa9fa421fd332e01ea3
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-06-05 19:16:00 +0500

    Show field.label in LabelComponent.

--- src/app/gorico/dynamic-forms/components/label/label.component.ts
+++ src/app/gorico/dynamic-forms/components/label/label.component.ts
@@ -3,15 +3,7 @@ import { FormGroup } from "@angular/forms";
 import { FieldConfig } from "../../field.interface";
 @Component({
   selector: "app-label",
-  template: `
-<mat-form-field *ngIf="field.isVisible != false" [ngStyle]="{'margin-right': '1%', 'margin-left': '1%','width': field.width+'%'}" appearance="outline" [formGroup]="group">
-<mat-label>{{field.label}}</mat-label>
-<label matInput [formControlName]="field.name" [readonly]="field.readonly" matTextareaAutosize matAutosizeMinRows="1" matAutosizeMaxRows="5"></label>
-<ng-container *ngFor="let validation of field.validations;" ngProjectAs="mat-error">
-<mat-error *ngIf="group.get(field.name).hasError(validation.name)">{{validation.message}}</mat-error>
-</ng-container>
-</mat-form-field>
-`,
+  template: `<p>{{field.label}}</p>`,
   styles: []
 })
 export class LabelComponent implements OnInit {
