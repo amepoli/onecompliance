fdca6535d3dcb40d52abda9cd2f1430434c67f57
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-06-06 22:00:01 +0500

    Show mat-label in Label Component.

--- src/app/gorico/dynamic-forms/components/label/label.component.ts
+++ src/app/gorico/dynamic-forms/components/label/label.component.ts
@@ -3,7 +3,9 @@ import { FormGroup } from "@angular/forms";
 import { FieldConfig } from "../../field.interface";
 @Component({
   selector: "app-label",
-  template: `<p>{{field.label}}</p>`,
+  template: `
+  <mat-label>{{field.label}}</mat-label>
+  `,
   styles: []
 })
 export class LabelComponent implements OnInit {
