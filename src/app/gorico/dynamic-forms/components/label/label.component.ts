3babe78e7d21d455e6a7ce97c793b8e8d68b22c6
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-06-11 18:03:12 +0500

    Improved Label Component.

--- src/app/gorico/dynamic-forms/components/label/label.component.ts
+++ src/app/gorico/dynamic-forms/components/label/label.component.ts
@@ -4,16 +4,18 @@ import { FieldConfig } from "../../field.interface";
 @Component({
   selector: "app-label",
   template: `
-  <div *ngIf="field.isVisible != false" [ngStyle]="{'margin-bottom': '24px', 'margin-right': '0%', 'margin-left': '2%','width': field.width+'%'}" appearance="outline" [formGroup]="group">
-  
-    <mat-label [style.background-color]="field.style.background_color" [style.color]="field.style.font_color" >{{field.label}}</mat-label>
-</div>
+  <div *ngIf="field.isVisible != false" [ngStyle]="{ 'background-color': field.style.background_color? field.style.background_color: 'transparent', 'border-radius':'4px', 'padding': '8px', 'margin-bottom': '24px', 'margin-right': '1%', 'margin-left': '1%','width': field.width+'%'}" appearance="outline" [formGroup]="group">
+    <mat-label [ngStyle]="{ 'color': field.style.font_color? field.style.font_color: 'black' , 'font-size': field.style.font_size? field.style.font_size: '1em', 'font-style': field.style.font_style? field.style.font_style: 'normal', 'font-weight': field.style.font_weight? field.style.font_weight: 'normal'}">
+      {{field.label}}
+    </mat-label>
+  </div>
   `,
   styles: []
 })
 export class LabelComponent implements OnInit {
   field: FieldConfig;
   group: FormGroup;
+
   constructor() { }
   ngOnInit() { }
 }
