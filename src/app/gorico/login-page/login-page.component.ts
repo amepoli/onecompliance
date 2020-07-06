653af234bc0dcd1635d8dc815b26eac0e4b6cd74
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-07-06 10:21:39 +0500

    Added login button text for information.

--- src/app/gorico/login-page/login-page.component.ts
+++ src/app/gorico/login-page/login-page.component.ts
@@ -22,6 +22,10 @@ export class LoginPageComponent implements OnInit {
     signingIn: boolean = false;
     loadingSession: boolean = false;
 
+    // Text to show on Login button
+    loginButtonText = 'LOGIN';
+
+    // Url used to return to after successfully logging in
     returnUrl = '/gorico/dashboard';
 
     /**
@@ -82,9 +86,7 @@ export class LoginPageComponent implements OnInit {
 
         this.authService.authStateChange$
             .subscribe(authState => {
-                this._dialogService.closeDialog();
                 this.signedIn = authState.state === 'signedIn';
-
                 if (!authState.user) {
                     this.user = null;
                 } else {
@@ -99,11 +101,13 @@ export class LoginPageComponent implements OnInit {
                 if (this.signingIn) {
                     this._dialogService.showErrorDialog("Error", err.message ? err.message : "Incorrect username or password");
                     this.signingIn = false;
+                    this.loginButtonText = 'LOGIN';
                 }
                 else {
                     // We failed to load previous session
                     this._dialogService.closeDialog();
                     this.loadingSession = false;
+                    this.loginButtonText = 'LOGIN';
                 }
                 // console.error(`Login Error: ${err}`);
             });
@@ -113,6 +117,7 @@ export class LoginPageComponent implements OnInit {
         this.authService.userinfo.subscribe(info => {
             if (info.username != null) {
                 // got info from backend, now we can proceed
+                this._dialogService.closeDialog();
                 this.router.navigate([this.returnUrl]);
             }
         });
@@ -121,6 +126,7 @@ export class LoginPageComponent implements OnInit {
         if (this.authService.doesAccessTokenExist()) {
             // Load session from local storage
             this.loadingSession = true;
+            this.loginButtonText = 'PLEASE WAIT';
             this._dialogService.showLoadingDialog("Loading", "Please wait...");
             this.authService.loadSession();
         }
@@ -130,6 +136,9 @@ export class LoginPageComponent implements OnInit {
     onSubmit(): void {
         this.authService.setUsername(this.loginForm.value.username);
         this.authService.setPassword(this.loginForm.value.password);
+
+        this.loginButtonText = 'PLEASE WAIT';
+
         // Show loading Alert
         this._dialogService.showLoadingDialog("Signing in", "Please wait...");
 
