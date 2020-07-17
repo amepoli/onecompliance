3c467418a58d24833dbf2067c494359d77b90ac8
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-07-17 11:55:11 +0500

    Aded email related lambda and dynamo table.

---
+++ dynamo-tables/email_templates/push_tables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#run 'npm install --global json-dynamo-putrequest' before launching the script the first time 
+files=(*.json)
+n=25          	#only 25 files can be processed as batch
+for ((i=0; i < ${#files[@]}; i+=n)); do
+	(echo "["; for f in "${files[@]:i:n}"; do (cat "$f"; echo ","); done; echo "]") | json-dynamo-putrequest --beautify email_templates > dynamo-input/dynamo_"$i".json
+done
+dynamo_files=(dynamo-input/*.json)
+for d in "${dynamo_files[@]}"; do
+    aws dynamodb batch-write-item --request-items file://$d  
+done
---
+++ lambda/email_trigger/deploy_lambda.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Please provide the target environment [gorico_prod, gorico_test, gorico_dev, xxx_prod, xxx_test, xxx_dev]"
+    exit 0
+fi
+
+LAMBDANAME="email_trigger"
+
+DYN_USERSNAME="users"
+DYN_PROFILESNAME="profiles"
+DYN_VIEWSNAME="views"
+
+DBNAME=""
+HOSTNAME="goricotest-new.caxbbckt9xen.eu-central-1.rds.amazonaws.com"
+
+if [ $1 == "gorico_dev" ]
+  then
+    DBNAME="Gorico"
+  else
+    LAMBDANAME="${LAMBDANAME}_$1"
+    DYN_USERSNAME="${DYN_USERSNAME}_$1"
+    DYN_PROFILESNAME="${DYN_PROFILESNAME}_$1"
+    DYN_VIEWSNAME="${DYN_VIEWSNAME}_$1"
+fi
+
+#replace Variables
+cp index.js index.js.ori
+
+sed -i -e "s/DB_NAME/${DBNAME}/g" index.js
+sed -i -e "s/HOST_NAME/${HOSTNAME}/g" index.js
+sed -i -e "s/USERS_NAME/${DYN_USERSNAME}/g" index.js
+sed -i -e "s/PROFILES_NAME/${DYN_PROFILESNAME}/g" index.js
+sed -i -e "s/VIEWS_NAME/${DYN_VIEWSNAME}/g" index.js
+
+rm index.js-e
+
+#push zip to AWS
+
+rm ./nodejs.zip
+
+zip -r nodejs.zip node_modules index.js package.json
+
+aws lambda update-function-code --function-name $LAMBDANAME --zip-file fileb://./nodejs.zip
+
+#restore the original file
+
+mv index.js.ori index.js
