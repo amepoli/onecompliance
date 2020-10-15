46c2b5750be5803a1a49dcde5af9febff19b5526
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-10-15 23:09:14 +0500

    Added margin in subform.

--- src/app/gorico/dynamic-forms/components/subform/subform.component.ts
+++ src/app/gorico/dynamic-forms/components/subform/subform.component.ts
@@ -14,12 +14,12 @@ import { FieldConfig } from '../../field.interface';
 `,
   styles: [`
     .subform-style {
-      margin-right: 0%;
-      margin-left: 0%;
+      margin-right: 1%;
+      margin-left: 1%;
       display: flex;
       flex-direction: row;
       flex-wrap: wrap;
-      width: 100%;
+      width: 98%;
     }
   `],
   host: {
