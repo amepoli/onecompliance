5ba5737ffe7481fd68b415b1358861ab98bfb3f1
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-10-19 21:01:37 +0500

    Updated email_trigger lambda and dynamodb tables.

--- lambda/email_trigger/index.js
+++ lambda/email_trigger/index.js
@@ -16,7 +16,7 @@ const pool = new Pool({
     connectionTimeoutMillis: 1000
 });
 
-var sender_address = 'amedeo.poli@auditft.it';
+var sender_address = 'amedeo.poli@alacritas.eu';
 
 // Some test addresses
 //var admin_addresses = ['akhtar.syedzeeshan@alacritas.eu', 'amedeo.poli@alacritas.eu', 'nicola.capovilla@alacritas.eu'];
