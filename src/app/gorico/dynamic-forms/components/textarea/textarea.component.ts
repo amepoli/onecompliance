110a6a5b51784f8429a03ddc0ed6011131859957
jzorjnz <akhtar.syedzeeshan@gmail.com>
2020-11-03 12:13:55 +0500

    Added background color.

--- src/app/gorico/dynamic-forms/components/textarea/textarea.component.ts
+++ src/app/gorico/dynamic-forms/components/textarea/textarea.component.ts
@@ -15,6 +15,11 @@ import { FieldConfig } from "../../field.interface";
 </mat-form-field>
 `,
   styles: [`
+    : host :: ng - deep.mat - form - field - flex {
+      background- color: aliceblue;
+      border- radius: 8px;
+    }
+  
     .text-area-s {
       min-height: 18px !important;
       max-height: 18px !important;
