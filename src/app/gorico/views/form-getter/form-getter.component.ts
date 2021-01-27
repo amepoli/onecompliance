5c1758f7a65f4641a3e9d7a9fcac62bc49a07fef
nikapov <nicola.capovilla@gmail.com>
2021-01-27 19:52:15 +0100

    Optimization of forEach

--- src/app/gorico/views/form-getter/form-getter.component.ts
+++ src/app/gorico/views/form-getter/form-getter.component.ts
@@ -222,21 +222,23 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
 
     public runOnReloadEvents() {
         if (this.outputEvents != null && this.outputEvents.length) {
-            this.outputEvents.forEach(outputEvent => {
+            for (let i=0; i < this.outputEvents.length; i++) {
+                const outputEvent = this.outputEvents[i];
                 if (!outputEvent.eventTrigger || outputEvent.eventTrigger === 'onReload') {
                     this.pubsubService.publishEvent(outputEvent.eventName, { origin: 'table', index: 0, data: this.filteredFormData, type: 'page' });
                 }
-            });
+            }
         }
     }
 
     public runOnSaveEvents() {
         if (this.outputEvents != null && this.outputEvents.length) {
-            this.outputEvents.forEach(outputEvent => {
+            for (let i=0; i < this.outputEvents.length; i++) {
+                const outputEvent = this.outputEvents[i];
                 if (!outputEvent.eventTrigger || outputEvent.eventTrigger === 'onSave') {
                     this.pubsubService.publishEvent(outputEvent.eventName, { origin: 'table', index: 0, data: this.filteredFormData, type: 'page' });
                 }
-            });
+            }
         }
     }
 
@@ -297,18 +299,20 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
                     // Get View properties if exist
                     _this.formRowProperties = params.formRowProperties;
                     if (_this.formRowProperties && _this.formRowProperties.length) {
-                        _this.formRowProperties.forEach(formRowProperty => {
+                        for (let i=0; i < _this.formRowProperties.length; i++) {
+                            const formRowProperty = _this.formRowProperties[i];
                             // Subscribe to all the input Events
                             if (formRowProperty.inputEvents && formRowProperty.inputEvents.length) {
-                                formRowProperty.inputEvents.forEach(event => {
+                                for (let j=0; j < formRowProperty.inputEvents.length; j++) {
+                                    const event = formRowProperty.inputEvents[j];
                                     const subcription = _this.pubsubService.subscribe(event.eventName,
                                         value => {
                                             _this.eventCallback(event, value, null); // null as keyListener means that the full table is affected
                                         });
                                     _this.formSubscriptions.push(subcription);
-                                });
+                                }
                             }
-                        });
+                        }
                     }
 
                     _this.currentKeys = _this.getCurrentKeys(_this.viewKeys, _this.formParams.keys);
@@ -317,13 +321,14 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
                     if (_this.firstRefresh || reloadEvents) {
                         _this.firstRefresh = false;     // avoid to subscribe to events again when refreshed
                         if (params.inputEvents != null) {  // subscribe to global table events
-                            params.inputEvents.forEach(event => {
+                            for (let i=0; i < params.inputEvents.length; i++) {
+                                const event = params.inputEvents[i];
                                 const subcription = _this.pubsubService.subscribe(event.eventName,
                                     value => {
                                         _this.eventCallback(event, value, null); // null as keyListener means that the full table is affected
                                     });
                                 _this.formSubscriptions.push(subcription);
-                            });
+                            }
                         }
                         _this.subscribeFieldInputEvents(_this.viewKeys);
                     }
@@ -368,19 +373,21 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
 
     subscribeFieldInputEvents(viewKeys: formViewKey[]): void {
         const _this = this;
-        viewKeys.forEach(key => {    // subscribe to single field events
+        for (let i=0; i < viewKeys.length; i++) { // subscribe to single field events
+            const key = viewKeys[i];
             if (key.inputEvents != null) {
-                key.inputEvents.forEach(event => {
+                for (let i=0; i < key.inputEvents.length; i++) {
+                    const event = key.inputEvents[i];
                     const subscription = _this.pubsubService.subscribe(event.eventName, value => {
                         _this.eventCallback(event, value, key.key);
                     });
                     _this.formSubscriptions.push(subscription);
-                });
+                }
             }
             if (key.format.viewType === 'subform' && key.format.subform_keys != null) {
                 _this.subscribeFieldInputEvents(key.format.subform_keys);
             }
-        });
+        }
     }
 
     getCurrentKeys(validKeysArray: formViewKey[], inputKeys: any) {
@@ -485,9 +492,10 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
                     result = result.data;
                     // add passed keys, if any - useful to valorize father's keys in subtables
                     if (default_keys != null) {
-                        result.forEach(element => {
+                        for (let i=0; i < result.length; i++) {
+                            let element = result[i];
                             element = Object.assign(element, default_keys);
-                        });
+                        }
                     }
                     // update the status to prevent the whole table refresh
                     _this.addingNew = true;
@@ -529,9 +537,10 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
         // field is of type '(key1,key2)', get the array
         try {
             const subKeysArray = commaSeparatedValues.split('(')[1].split(')')[0].split(',');
-            subKeys.forEach((subKey, sub_index) => {
-                outputObject[subKey.key] = subKeysArray[sub_index];
-            });
+            for (let i=0; i < subKeys.length; i++) {
+                const subKey = subKeys[i];
+                outputObject[subKey.key] = subKeysArray[i];
+            }
         } catch (e) {
             this._console.log('something wrong with subkeys');
         }
@@ -541,17 +550,19 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
     private getFieldValues(formKeys: formViewKey[], values: any, index: number): FieldConfig[] {
         const _this = this;
         const fieldValues = new Array();
-        formKeys.forEach(field => {
+        for (let i=0; i < formKeys.length; i++) {
+            const field = formKeys[i];
             if (field != null) {
                 const element = values[index][field.key];
                 // process subkeys of combos/radiobuttons/etc.
                 if (field.subKeys != null && field.subKeys.length > 0) {
                     if (element.options != null) {
-                        element.options.forEach(option => {
+                        for (let j=0; j < element.options.length; j++) {
+                            const option = element.options[j];
                             if (option.id != null) {
                                 option.id = _this.getSubKeysObject(field.subKeys, option.id);
                             }
-                        });
+                        }
                     }
                     if (element.value != null) {
                         element.value = _this.getSubKeysObject(field.subKeys, element.value);
@@ -563,7 +574,7 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
                     fieldValues.push(fieldValue);
                 }
             }
-        });
+        }
         return fieldValues;
     }
 
@@ -735,13 +746,15 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
             // Check if formRowProperties contains keys 
             if (event.keys != null) {
                 // Check each form table line to see if the condition is met
-                _this.filteredFormData.forEach((formKeys, i) => {
+                for (let i=0; i < _this.filteredFormData.length; i++) {
+                    const formKeys = _this.filteredFormData[i];
 
                     let matchingKeys = true;
                     let matchingValues = true;
 
                     // Run for each key
-                    event.keys.forEach(key => {
+                    for (let j=0; j < event.keys.length; j++) {
+                        const key = event.keys[j];
 
                         let senderValue;
                         const receiverEntry: any = formKeys.find(x => x.label === key.receiver);
@@ -760,7 +773,7 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
                                 matchingKeys = false;
                             }
                         }
-                    });
+                    }
 
                     // Perform action based on conditions check above
                     if (!matchingKeys) {
@@ -769,7 +782,7 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
 
                     _this.hiddenRows[i] = !matchingValues;
 
-                });
+                }
             }
             return;
         }
@@ -845,7 +858,8 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
             }, {});
             let filteredKeys = {};
             if (event.actionTarget.keymap != null && event.actionTarget.keymap.length) { // explicit key map between tables
-                event.actionTarget.keymap.forEach(element => {
+                for (let i=0; i < event.actionTarget.keymap.length; i++) {
+                    const element = event.actionTarget.keymap[i];
                     if (element.source != null && element.destination != null) {
                         // Check if event contains values in case of manually generated event
                         if (event.values != null) {
@@ -856,7 +870,7 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
                         }
                     }
 
-                });
+                }
             }
             else {
                 const primaryKeys = _this.viewKeys.filter(key => key.isPrimary);
@@ -1124,7 +1138,7 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
 
                     if (entry && Object.entries(entry).length) {
                         // Since we will use fullValueSet, we don't need to read all fields
-                        Object.entries(entry).forEach(([key, value]) => {
+                        for (const [key, value] of Object.entries(entry)) {
                             // console.log(key, value);
                             if (value && !done) {
                                 let dataType = typeof (value);
@@ -1157,7 +1171,7 @@ export class FormGetterComponent implements OnChanges, AfterViewInit, OnDestroy 
                                     // code here...
                                 }
                             }
-                        });
+                        }
 
                     }
                     return add;
