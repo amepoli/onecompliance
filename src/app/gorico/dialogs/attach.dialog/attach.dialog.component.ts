8a9224845488c29f4e7c5c8ccac8e304fa07fb86
jzorjnz <akhtar.syedzeeshan@gmail.com>
2021-03-19 20:07:59 +0500

    Use crypto-js library for hashing.

--- src/app/gorico/dialogs/attach.dialog/attach.dialog.component.ts
+++ src/app/gorico/dialogs/attach.dialog/attach.dialog.component.ts
@@ -6,13 +6,16 @@ import { BackendService } from 'app/gorico/views/backend/backend.service';
 import { saveAs } from 'file-saver';
 import { HttpClient } from '@angular/common/http';
 import { FileUploadComponent } from 'app/gorico/file-uploader/file-upload/file-upload.component';
-import { createHash } from 'crypto';    // pls. read https://stackoverflow.com/questions/54162297/module-not-found-error-cant-resolve-crypto
+// import { createHash } from 'crypto';    // pls. read https://stackoverflow.com/questions/54162297/module-not-found-error-cant-resolve-crypto
 // and https://stackoverflow.com/a/54645398 and then 'npm run build'
+import * as CryptoJS from 'crypto-js';
+
 import { formGetterParams, FormGetterComponent } from 'app/gorico/views/form-getter/form-getter.component';
 import { AuthService } from 'app/gorico/login-page/auth.service';
 import { Subscription } from 'rxjs';
 import { ToastService } from 'app/gorico/services/toast.service';
 import { ConsoleLoggerService } from 'app/gorico/services/console_logger.service';
+
 
 
 @Component({
@@ -256,9 +259,12 @@ export class AttachDialogComponent implements OnInit, AfterViewInit, OnDestroy {
                                     for (var i = 0; i < content.byteLength; i++) {
                                          buffer[i] = content[i];
                                     };*/
-                                    var buffer = Buffer.from(<string>content);
                                     // create file content hash
-                                    const hash = createHash('sha1').update(buffer).digest("hex");
+                                    // Old method
+                                    // var buffer = Buffer.from(<string>content);
+                                    // const hash = createHash('sha1').update(buffer).digest("hex");
+                                    // New methd by Zee
+                                    const hash = CryptoJS.SHA1(<string>content).toString(CryptoJS.enc.Hex);
                                     _this._console.log(hash);
                                     // check that the file has been correctly uploaded and pass file params to the backend
                                     var mime = require('mime-types');
