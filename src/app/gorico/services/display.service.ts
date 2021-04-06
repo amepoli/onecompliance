5142fa1262b705a41c1d55f12eba8baf71037687
jzorjnz <akhtar.syedzeeshan@gmail.com>
2021-04-06 22:20:50 +0500

    Added Display Service.

---
+++ src/app/gorico/services/display.service.ts
@@ -0,0 +1,81 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+export interface DisplayServiceConstants {
+    navbarHeight: number,
+    mainTableHeaderHeight: number,
+    formViewMiniHeight: number,
+    bottomTabsHeaderHeight: number,
+};
+
+export interface DisplayServiceSettings {
+    mainTableViewContentHeight: number,
+    bottomViewHeight: number,
+};
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DisplayService {
+
+    public constants: DisplayServiceConstants = {
+        navbarHeight: 64,
+        mainTableHeaderHeight: 110,
+        formViewMiniHeight: 480,
+        bottomTabsHeaderHeight: 50
+    };
+
+    public settings: DisplayServiceSettings  = {
+        mainTableViewContentHeight: 0,
+        bottomViewHeight: 0
+    };
+
+    public windowHeight: number = 0;
+
+    /**
+     * DisplayServiceSettingsEventEmitter
+     */
+    public onDisplayServiceSettingsChanged = new EventEmitter<DisplayServiceSettings>();
+
+    /**
+     * Constructor
+     *
+     */
+    constructor() {
+        this.windowHeight = window.innerHeight;
+        this.createSingleView('table');
+    }
+
+    public createSingleView(tableType) {
+        if(tableType === 'table') {
+            this.settings = {
+                mainTableViewContentHeight: this.windowHeight - this.constants.navbarHeight,
+                bottomViewHeight: 0
+            }
+        }
+        else {
+            this.settings = {
+                mainTableViewContentHeight: this.windowHeight - this.constants.navbarHeight - 110,
+                bottomViewHeight: 0
+            }
+        }
+        this.onDisplayServiceSettingsChanged.emit(this.settings);
+    }
+
+    public createMultiView() {
+        this.settings = {
+            mainTableViewContentHeight: this.constants.formViewMiniHeight - 10,
+            bottomViewHeight: this.windowHeight - this.constants.navbarHeight - this.constants.mainTableHeaderHeight - this.constants.bottomTabsHeaderHeight - this.constants.formViewMiniHeight - 24
+        }
+        this.onDisplayServiceSettingsChanged.emit(this.settings);
+    }
+
+    public createFullscreenMultiView() {
+        this.settings = {
+            mainTableViewContentHeight: 0,
+            bottomViewHeight: this.windowHeight - this.constants.mainTableHeaderHeight - this.constants.bottomTabsHeaderHeight - 10
+        }
+        this.onDisplayServiceSettingsChanged.emit(this.settings);
+    }
+
+}
+
