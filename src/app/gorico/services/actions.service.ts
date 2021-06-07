3a85eb0b72b0941318faea84fed1ba546a81fdf2
jzorjnz <akhtar.syedzeeshan@gmail.com>
2021-06-07 20:42:45 +0500

    Show form action dialog when message provided.

--- src/app/gorico/services/actions.service.ts
+++ src/app/gorico/services/actions.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { SweetAlertResult } from 'sweetalert2';
 import * as appData from '../../../../appdata.json';
 import { ILoggerService, MessageElement, MessageItem } from '../interfaces';
 import { FormActionType } from '../types';
@@ -21,48 +22,48 @@ export class ActionsService {
 
     }
     public getFormActionMessage(actionType: FormActionType, messages: MessageElement[]) {
-        let result: MessageItem = {
-            title: actionType,
-            text: "Are you sure you want to perform action?"
-        };
-        if (messages != null && messages.length) {
+        let result: MessageItem = null;
+        if (messages != null && messages.length > 0) {
             let messageElements = messages.filter(m => m.messageType === actionType);
-            if (messageElements && messageElements.length) {
-                result.title = messageElements[0].message.title;
-                result.text = messageElements[0].message.text;
+            if (messageElements && messageElements.length > 0) {
+                result = {
+                    title: messageElements[0].message.title,
+                    text: messageElements[0].message.text
+                };
             }
         }
         return result;   
     }
 
-    public performFormAction(actionType: FormActionType, messages: MessageElement[], entryName: string, company: string, keys: any, ){
+    public async performFormAction(actionType: FormActionType, messages: MessageElement[], entryName: string, company: string, keys: any, ){
         var _this = this;
         let message: MessageItem = _this.getFormActionMessage(actionType, messages);
-
-        // Show confirmation dialog to make sure user wants to perform action
-        _this._dialogService.showConfimationDialog(message.title, message.text, "Yes", "No", "warning").then((result) => {
-            if (result.value === true) {
-                // User said yes so let's perform action
-                const subscription = _this.backendService.performFormAction(actionType, entryName, company, keys ).subscribe(
-                    result => {
-                        _this._console.log(result);
-                        if (result.result === 'OK') {
-                            // Show success toast
-                            _this._toastService.showSuccessToast(actionType + " successful!");
-                        }
-                        else {
-                            // Show error snackbar
-                            _this._toastService.showErrorToast(result.reason);
-                        }
-                        subscription.unsubscribe();
-                    },
-                    error => {
-                        // Show error snackbar
-                        _this._toastService.showErrorToast(error);
-                        subscription.unsubscribe();
-                    }
-                );
+        if(message) {
+            // Show confirmation dialog to make sure user wants to perform action
+            let dialogResult: SweetAlertResult<any> = await _this._dialogService.showConfimationDialog(message.title, message.text, "Yes", "No", "warning");
+            if(!dialogResult.isConfirmed) {
+                return;
             }
-        });
+        }
+
+        const subscription = _this.backendService.performFormAction(actionType, entryName, company, keys ).subscribe(
+            result => {
+                _this._console.log(result);
+                if (result.result === 'OK') {
+                    // Show success toast
+                    _this._toastService.showSuccessToast(actionType + " successful!");
+                }
+                else {
+                    // Show error snackbar
+                    _this._toastService.showErrorToast(result.reason);
+                }
+                subscription.unsubscribe();
+            },
+            error => {
+                // Show error snackbar
+                _this._toastService.showErrorToast(error);
+                subscription.unsubscribe();
+            }
+        );
     }
 }
