bf7ff765088ac0857a32b0339d13b703bc26d263
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2021-11-03 16:57:49 +0500

    Updated Google API.

--- lambda/google_api/index.js
+++ lambda/google_api/index.js
@@ -1,54 +1,15 @@
 const https = require('https');
 const helperFuncts = require('./helperFuncts');
-const googleAPI = 'GOOGLE_API_KEY';
-const CLIENT_ID = '380240687769-t5gsbc7upsc82fdsihll6svpk16sujkg.apps.googleusercontent.com';
-const CLIENT_SECRET = 'F56L14ZUTbykHAdTTSiAPBUb';
 const { Client } = require("@googlemaps/google-maps-services-js");
 const googleMapsClient = new Client({});
 const { OAuth2Client } = require('google-auth-library');
 const oAuth2Client = new OAuth2Client(
-    CLIENT_ID,
-    CLIENT_SECRET
+    'CLIENT_ID',
+    'CLIENT_SECRET'
 );
 // var promisify = require('promisify');
 // const { promisify } = require('bluebird');
 const { google } = require('googleapis');
-
-
-const token =
-{
-    "id": "107619544484794949926",
-    "name": "Syed Zeeshan Akhtar",
-    "email": "akhtar.syedzeeshan@gmail.com",
-    "photoUrl": "https://lh3.googleusercontent.com/a-/AOh14GiZynR3rZ-yg2XKGK_i--uxV3Bch2TPyYdRSooo=s96-c",
-    "firstName": "Syed Zeeshan",
-    "lastName": "Akhtar",
-    "authToken": "ya29.a0ARrdaM-C2bAfXqVHqROuazYsnBF_IqMA7hpfTNwKJ1mdhUimqyhrtplXqqG0Me8X3s-ssLT8PvwGeLqhlXUQYo6sxqAfXOVN_Ymwwem7wnrfoqEGHYl5FFMKJHAFzu-Lr6TEdvwkAdVuiKUC-y3AOmkA2qRC",
-    "idToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFkZDhjMGVlNjIzOTU0NGFmNTNmOTM3MTJhNTdiMmUyNmY5NDMzNTIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiMzgwMjQwNjg3NzY5LXQ1Z3NiYzd1cHNjODJmZHNpaGxsNnN2cGsxNnN1amtnLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMzgwMjQwNjg3NzY5LXQ1Z3NiYzd1cHNjODJmZHNpaGxsNnN2cGsxNnN1amtnLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA3NjE5NTQ0NDg0Nzk0OTQ5OTI2IiwiZW1haWwiOiJha2h0YXIuc3llZHplZXNoYW5AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJtMmpJSnFVazhneFFPZG1fanFtWmVBIiwibmFtZSI6IlN5ZWQgWmVlc2hhbiBBa2h0YXIiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2laeW5SM3JaLXlnMlhLR0tfaS0tdXhWM0JjaDJUUHlZZFJTb29vPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IlN5ZWQgWmVlc2hhbiIsImZhbWlseV9uYW1lIjoiQWtodGFyIiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2MzQ2NDM1MDEsImV4cCI6MTYzNDY0NzEwMSwianRpIjoiY2QzNmM1MDJjMTY0Y2QxZWQ1ZjFmZTVjNjNhYmI3ODYxZTc5N2UzNCJ9.IWGnAwRF4_35PUZpWUVEQ0i2qGBv_j-k7p_OwR67qOwAVJiN7fDYLSO-4zj38850_8pSEz9sV7kpvrWAfe7DqdgFLkvERNRmqD5ztEjURSiUHu5xfIJvVeI8N_F16Zz4soCoVOmlojaEJFKA0QHznybdfOddGxySoQpXb-UjpMtIJp7b8Yj3OJm5NCezO-cYPHOHeYJj0bwKvcPdhGaqs0BcZXecv9fKPqFA1PpzUmjScBCFplrjf0poUvKAn8VcuWGGnkJfIpB7_S4ZeaRpy6ABdFfW8Ipppzvx2WzOm2-RJBaP1P-23Z2090HipGVOdMahiMejs0RgwAt3EIdxAA",
-    "response": {
-        "token_type": "Bearer",
-        "access_token": "ya29.a0ARrdaM_ymPu-HIZ2kPIwQvDFd2fILh0wlthv52W9ptm611LdWkARR8H3E9Nq5qpyyX67r_KwYsPpJT5mJyVwFVhAdJuCJ0txiyJT9HuUnXLUEa5JK_OfxIHbDijzMhiYvRmlRtJt1mtPqL-M7fl85YLQkPNK",
-        "scope": "email profile https://mail.google.com/ https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid",
-        "login_hint": "AJDLj6J9nXGC8nvyinFBHbJdEwBh5kZMnzvlvBUhjas9Mf1IzgBa5B9d6wEaWR4_vDbvrL_tKkZzX_EZOuE-o6YX8u5Z4TTSeg",
-        "expires_in": 3599,
-        "id_token": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImJiZDJhYzdjNGM1ZWI4YWRjOGVlZmZiYzhmNWEyZGQ2Y2Y3NTQ1ZTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiMzgwMjQwNjg3NzY5LXQ1Z3NiYzd1cHNjODJmZHNpaGxsNnN2cGsxNnN1amtnLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMzgwMjQwNjg3NzY5LXQ1Z3NiYzd1cHNjODJmZHNpaGxsNnN2cGsxNnN1amtnLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA3NjE5NTQ0NDg0Nzk0OTQ5OTI2IiwiZW1haWwiOiJha2h0YXIuc3llZHplZXNoYW5AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJQSDlLdG1aT1BrYkNqc0lrNTQwdUNnIiwibmFtZSI6IlN5ZWQgWmVlc2hhbiBBa2h0YXIiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2laeW5SM3JaLXlnMlhLR0tfaS0tdXhWM0JjaDJUUHlZZFJTb29vPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IlN5ZWQgWmVlc2hhbiIsImZhbWlseV9uYW1lIjoiQWtodGFyIiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2MzQ4OTYwODYsImV4cCI6MTYzNDg5OTY4NiwianRpIjoiZGU5NDNkODNkOTk3OTg4MGM5ZDEyNTk0MzY5NTg4ZmQwYjVkZDViYyJ9.k87QpwEb_-ThOKmeC_WIe5mZS6XyyGunMpM4R3_c6UJA-Lk6oeUYH_F5wugwcrdOqTEbmoLjCcWLroKx0kkqxcni3AeqsRFeP-EKHWeo_5HpFWFL3g29BXYcCcA13_IIgJGhDinJI9L-Z5IR5yiOlvWpmNeKtxiIhJrKeUBI4lM-TChjLrwSyBiJeKYnOYNq2jniMyMDt8TJNPVGWGo695aCwtUYi2y742YxLw9w9ldivRG4zsQ-yo_e3MxY1Pl-KhdVypup4wUgNLfWCdZtcZg9kBUjpmnfTJ8lQZCKVYc7yPuzDsdGgG_fTflEWd0msvzrqi-ZKQwr-ZAG4x33RA",
-        "session_state": {
-            "extraQueryParams": {
-                "authuser": "0"
-            }
-        },
-        "first_issued_at": 1634896086564,
-        "expires_at": 1634899685564,
-        "idpId": "google",
-
-
-        "refresh_token": 'ya29.a0ARrdaM_ymPu-HIZ2kPIwQvDFd2fILh0wlthv52W9ptm611LdWkARR8H3E9Nq5qpyyX67r_KwYsPpJT5mJyVwFVhAdJuCJ0txiyJT9HuUnXLUEa5JK_OfxIHbDijzMhiYvRmlRtJt1mtPqL-M7fl85YLQkPNK',
-        "code": "4/0AX4XfWiIpDqZDo_3UgFm6sTL5AuWRRxNd0jcxMc8p9rZOhxQuNZUijREH5HrI4yxLF7G3Q",
-        "client_secret": 'F56L14ZUTbykHAdTTSiAPBUb',
-        "grant_type": "authorization_code"
-    },
-    "provider": "GOOGLE"
-};
 
 // Get Bad URL Response
 function getBadUrlResponse() {
@@ -87,7 +48,7 @@ async function getDirections(queryParams) {
             origin: queryParams['origin'],
             destination: queryParams['destination'],
             travel_mode: 'DRIVING',
-            key: googleAPI
+            key: 'GOOGLE_API_KEY'
         }
     };
     const response = await googleMapsClient.directions(params);
@@ -107,7 +68,7 @@ async function getDistance(queryParams) {
             origins: [queryParams['origin']],
             destinations: [queryParams['destination']],
             travel_mode: 'DRIVING',
-            key: googleAPI
+            key: 'GOOGLE_API_KEY'
         }
     };
     const response = await googleMapsClient.distancematrix(params);
@@ -121,7 +82,15 @@ async function getDistance(queryParams) {
 }
 
 // Get Email Threads
-async function getEmailThreads(queryParams) {
+async function getEmailThreads(queryParams, authParams) {
+    const search = queryParams['search'];
+    authParams["refresh_token"] = authParams["access_token"];
+    //authParams["code"] = "4/0AX4XfWiIpDqZDo_3UgFm6sTL5AuWRRxNd0jcxMc8p9rZOhxQuNZUijREH5HrI4yxLF7G3Q",
+    authParams["client_secret"] = 'CLIENT_SECRET';
+    console.log('authParams: ', authParams);
+
+    // authParams["grant_type"] = "authorization_code"
+
     // const auth = new google.auth.fromJSON(token);
 
     // .auth.GoogleAuth({
@@ -139,14 +108,14 @@ async function getEmailThreads(queryParams) {
 
 
     const ticket = await oAuth2Client.verifyIdToken({
-        idToken: token.response.id_token,
-        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        idToken: authParams.id_token,
+        audience: 'CLIENT_ID',  // Specify the CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
     });
     console.log('ticket: ', ticket);
 
-    oAuth2Client.setCredentials(token.response);
+    oAuth2Client.setCredentials(authParams);
     // oAuth2Client.setCredentials({
     //     access_token: token.response.access_token,
     //     refresh_token: '4/0AX4XfWgrxymEqB8qeeD6m8LTuoAqpYf6K7J-kHH7XPNH9l8uYjBqpSz2EGrHzsOkg1ouoA',
@@ -155,7 +124,7 @@ async function getEmailThreads(queryParams) {
     // });
 
     // after acquiring an oAuth2Client...
-    const tokenInfo = await oAuth2Client.getTokenInfo(token.response.access_token);
+    const tokenInfo = await oAuth2Client.getTokenInfo(authParams.access_token);
     // take a look at the scopes originally provisioned for the access token
     console.log('tokenInfo: ', tokenInfo);
 
@@ -269,7 +238,7 @@ exports.handler = async (event, context) => {
     console.log('queryParams: ', queryParams);
 
     const requestType = queryParams['request_type'];
-
+    
     // If no Request type provided, exit with an error
     if (!requestType) {
         return getBadUrlResponse();
@@ -287,7 +256,7 @@ exports.handler = async (event, context) => {
                 body = await getDistance(queryParams);
             }
             else if (requestType === 'GetEmailThreads') {
-                body = await getEmailThreads(queryParams);
+                body = await getEmailThreads(queryParams, event.body? JSON.parse(event.body): {});
             }
             else {
                 return getBadUrlResponse();
