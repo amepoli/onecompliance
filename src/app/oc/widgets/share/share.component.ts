d60a1d5b781c158957a7d2e725f1892939e9a6d6
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2022-01-05 19:06:16 +0500

    Created share widget

---
+++ src/app/oc/widgets/share/share.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit, DoCheck, OnChanges, Output, EventEmitter } from '@angular/core';
+import { AuthService } from 'app/oc/services';
+
+import 'rxjs/add/operator/filter';
+import { FormViewComponent } from '../../views/form/form-view.component';
+
+@Component({
+    selector: 'share',
+    templateUrl: './share.component.html',
+    styleUrls: ['./share.component.scss']
+})
+export class ShareComponent {
+
+    @Output() onClick = new EventEmitter<any>();
+
+    userdata: any;
+    userCompanies: string[] = [];
+    
+    constructor(private _authService: AuthService) {
+        // get user data after login
+        this.userdata = this._authService.userinfo.getValue();
+        
+        // set the company set
+        this.userCompanies = this.userdata.companies;
+
+    }
+
+    performClick(item){
+        this.onClick.emit(item);
+    }
+}
