9cd3e87f13f2f7bcbfa60eb3d1e4074511d9cfbc
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2022-01-05 23:46:11 +0500

    Added widget dynamic component.

---
+++ src/app/oc/dynamic-forms/components/widget/widget.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FieldConfig } from 'app/oc/interfaces';
+import { ConsoleLoggerService, DialogService, HelperService, PubSubService, ValidationsService } from 'app/oc/services';
+import { TimezoneService } from 'app/oc/services/timezone.service';
+import * as moment from 'moment';
+import { Moment } from 'moment';
+import { Subscription } from 'rxjs';
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.scss'],
+  host: {
+    '[style.padding-top.px]': 'field.isVisible? "24": "0"',
+    '[style.margin-right.px]': 'field.isVisible? "8": "0"',
+    '[style.margin-left.px]': 'field.isVisible? "8": "0"',
+    '[style.width.px]': 'field.isVisible? "32": "0"',
+    '[style.height.px]': 'field.isVisible? "32": "0"',
+  }
+})
+export class WidgetComponent implements OnInit, AfterViewInit, OnDestroy {
+  field: FieldConfig;
+  group: FormGroup;
+  readOnlyPage: boolean = false; // field.readonly overridden by page
+  isRequired = false; // field is required or not
+
+  subscription: Subscription;
+
+  // For future use
+  // @HostBinding('style.margin-right') marginRight = '1%';
+
+  constructor(private timezoneService: TimezoneService,
+              private pubSubService: PubSubService,
+              private _console: ConsoleLoggerService,
+              private _dialogService: DialogService) { }
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+      if (this.subscription != null) {
+        this.subscription.unsubscribe();
+      }
+  }
+
+  
+}
