ebaf1609d20b3a9f8e8762bac00f9727471b4bb1
deidStorm <d.tonicello95@gmail.com>
2022-01-18 11:58:44 +0100

    fixed bug on eventtrigger change

--- src/app/oc/dynamic-forms/components/checkbox/checkbox.component.ts
+++ src/app/oc/dynamic-forms/components/checkbox/checkbox.component.ts
@@ -31,7 +31,7 @@ export class CheckboxComponent implements OnInit {
     if (typeof _this.field.value === 'string') {
       _this.field.value = parseInt(_this.field.value, 10);
     }
-    if (_this.field.eventName !== null && _this.field.eventTrigger === 'change') {
+    if (_this.field.eventName !== null && _this.field.eventTrigger === 'load') {
       if (!_this.field.conditionalQuery) {
         // No condition required, wait a while before triggering the event
         setTimeout(() => { _this.pubSubService.publishEvent(_this.field.eventName, { origin: _this.field.name, index: _this.field.index, valueSet: _this.field.fullValueSet, data: _this.field.value, type: 'change' }); }, 50);
