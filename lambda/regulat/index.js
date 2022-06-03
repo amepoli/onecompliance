c233427912e28e65c115448e912e01da811a98ae
deidStorm <d.tonicello95@gmail.com>
2022-06-03 12:54:55 +0200

    regulat/index.js first draft

--- lambda/regulat/index.js
+++ lambda/regulat/index.js
@@ -2,9 +2,8 @@ const https = require('https');
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'eu-central-1' });
 const lambda = new AWS.Lambda({
-    region: 'eu-central-1' //change to your region
+  region: 'eu-central-1'
 });
-
 
 function post(options, postData) {
   return new Promise(((resolve, reject) => {
@@ -33,186 +32,185 @@ function post(options, postData) {
   }));
 }
 
-
-
 exports.handler = async (event, context) => {
-    
 
-  //define post
+  //Declare queryParams
+  const entityType = queryParams['entityType'];
+  const name = queryParams['name'];
+  const surname = queryParams['surname'];
+  const yob = queryParams['yob'];
 
-    var postData_login = JSON.stringify({   
-        "strUser":"ALACRITAS",
-        "strPassword":"ALACRITAS",
-        "oConnectionInfo": {
-            "Language": "0",
-            "DateFormat": "dd/mm/yyyy",
-            "WorkflowDomain": "SIAV"
-        }
-      });
-    
-    var options_login = {
-      "method": "POST",
-      "hostname": "afmobile.finint.com",
-      "path": "/ArchiflowService/Login.svc/json/Login",
-      "headers": {
-        "Content-Type": "application/json",
-      }
-    };
-    
 
-/* 
-    var postData_getCards = {
-        "paramIn": {
-            "SessionInfo": {
-                "CharacterSet": 1,
-                "ClientType": 0,
-                "DateFormat": "dd/MM/yyyy",
-                "ExecutiveOfficeCode": 0,
-                "ExecutiveOffices": [],
-                "Language": 0,
-                "LoginTicketUserId": null,
-                "LoginType": 1,
-                "SessionId": "",
-                "TokenSess": "192.168.200.84",
-                "VelocisDatabase": "arcsql50",
-                "VelocisServer": "RDS",
-                "WorkflowId": ""
-            },
-            "SearchCriteria": {
-            },
-            "PageNumber": 1,
-            "PageSize": 1,
-            "GetIndexes": true,
-            "GetInvoice": false
-        }
-    };
-    
-    
-    var options_getCards = {
-      "method": "POST",
-      "hostname": "afmobile.finint.com",
-      "path": "/archiflowservice/Card.svc/json/RetrieveCardsByParam",
-      "headers": {
-        "Content-Type": "application/json",
-      }
-    };
-    
-    var tipoFornitoreFilter = ["CONSULENTE TECNICO", "CONSULENTE FISCALE", "LEGALE /AMMINISTRATIVO"];
+  //Configure Post for authenticate
+  var postData_login = JSON.stringify({
+    "username": "f7ee43b4-99ae-42d3-bf70-63507cc15055_test",
+    "password": "QXPTmDfw8ABMTfN6******************************M"
+  });
 
-    let numRecords = event.queryStringParameters.numRecords;
-
-    if (numRecords > 0) {
-        // recover a set of records
-        postData_getCards.paramIn.PageSize = numRecords;
-    } else {
-        // get the number of records
-        postData_getCards.GetIndexes = false;
+  var options_login = {
+    "method": "POST",
+    "hostname": "https://app.regulat.io",
+    "path": "/api/auth/token",
+    "headers": {
+      "Content-Type": "application/json",
     }
-    
- */
+  };
 
-    //get token
+  /* MAYBE SOMETHING LIKE THIS?
 
-    let response = await post(options_login, postData_login);
-    //let response = await post(options_test, postData_test);
-    
-    if (response == null || response.oSessionInfo == null ||  response.oSessionInfo.SessionId == null || response.oSessionInfo.WorkflowId == null) {
-        return({
-            "statusCode": 200, 
-            "isBase64Encoded": false,
-            "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-            "body": JSON.stringify({result: 'KO', reason: 'Something wrong with getting doc list'})
-          });
-    }
-    
-    let token = response.oSessionInfo.SessionId;
-    
+  var clientId = "MyApp";
+  var clientSecret = "MySecret";
 
+  // var authorizationBasic = $.base64.btoa(clientId + ':' + clientSecret);
+  var authorizationBasic = window.btoa(clientId + ':' + clientSecret);
 
+  var request = new XMLHttpRequest();
+  request.open('POST', oAuth.AuthorizationServer, true);
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  request.setRequestHeader('Authorization', 'Basic ' + authorizationBasic);
+  request.setRequestHeader('Accept', 'application/json');
+  request.send("username=John&password=Smith&grant_type=password");
 
-    /* 
-    let wid = response.oSessionInfo.WorkflowId;
-    
-    postData_getCards.paramIn.SessionInfo.SessionId = token;
-    
-    postData_getCards.paramIn.SessionInfo.WorkflowId = wid;
-    
-    postData_getCards = JSON.stringify(postData_getCards);
-    
-    response = await post(options_getCards, postData_getCards);
-    
-    if (response == null || response.RetrieveCardsByParamResult == null ||  response.RetrieveCardsByParamResult.Cards == null) {
-      return({
-          "statusCode": 200, 
-          "isBase64Encoded": false,
-          "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-          "body": JSON.stringify({result: 'KO', reason: 'Something wrong with getting doc list'})
-        });
-    }
-
-    if (numRecords === 0) {
-        // return the number of records and stop here
-        numRecords = response.RetrieveCardsByParamResult.HitCount;
-        return({
-            "statusCode": 200, 
-            "isBase64Encoded": false,
-            "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-            "body": JSON.stringify({result: 'OK', numRecords: numRecords})
-          });
-    }   
-
-    let cards = response.RetrieveCardsByParamResult.Cards;
-    
-    let processedCards = [];
-    
-    cards.forEach(card => {
-      let cardId = card.CardId;
-      let indexes = card.Indexes;
-      let progressivo = card.CardProg;
-      if (indexes != null) {
-        let dataFirma = '';
-        let societaFondo = '';
-        let controparte = '';
-        let piva = '';
-        let tipoFornitore = '';
-        let numSistema = '';
-        
-        indexes.forEach(index => {
-          if (index.FieldDescription === 'Data firma' ) {
-            dataFirma = index.FieldValue;
-          } else if (index.FieldDescription === 'Società/Fondo' ) {
-            societaFondo = index.FieldValue;
-          } else if (index.FieldDescription === 'Controparte' ) {
-            controparte = index.FieldValue;
-          } else if (index.FieldDescription === 'P.IVA' ) {
-            piva = index.FieldValue;
-          } else if (index.FieldDescription === 'Tipologia fornitore' ) {
-            tipoFornitore = index.FieldValue;
-          } else if (index.FieldDescription === 'N° di sistema' ) {
-            numSistema = index.FieldValue;
-          }
-        });
-        if (tipoFornitoreFilter.indexOf(tipoFornitore) > -1) {
-          processedCards.push({"cardId": cardId, "progressivo": progressivo, "dataFirma": dataFirma, "societaFondo": societaFondo, "controparte": controparte, "piva": piva, "tipoFornitore": tipoFornitore, "numSistema": numSistema});
-        }
+  request.onreadystatechange = function () {
+      if (request.readyState === 4) {
+        alert(request.responseText);
       }
+  };
+
+  */
+
+
+  //Configure Post for scan, it depends on the entityType (see the technical notes doc)
+  if (entityType == 'P') {
+    var postData_getScan = {
+      "access_token": "",
+      "refresh_token": "",
+      "firstname": "",
+      "lastname": "",
+      "yob": "",
+      "responseType": "json"
+    };
+    postData_getScan.firstname = name;
+    postData_getScan.lastname = surname;
+    postData_getScan.yob = yob;
+
+  } else if (entityType == 'E') {
+    var postData_getScan = {
+      "access_token": "",
+      "refresh_token": "",
+      "company": "",
+      "responseType": "json"
+    };
+    postData_getScan.company = name;
+
+  };
+
+  var options_getScan = {
+    "method": "POST",
+    "hostname": "https://app.regulat.io",
+    "path": "/api/auth/token",
+    "headers": {
+      "Content-Type": "application/json",
+    }
+  };
+
+
+  //Get token
+  let response = await post(options_login, postData_login);
+  //let response = await post(options_test, postData_test);
+
+  if (response == null || response.access_token == null) {
+    return ({
+      "statusCode": 200,
+      "isBase64Encoded": false,
+      "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+      "body": JSON.stringify({ result: 'KO', reason: 'Something wrong with getting access token' })
     });
-    
-    console.log(processedCards); */
+  }
+
+  let token = response.access_token;
 
 
-    
-    response = await lambda.invoke({
-        FunctionName: 'FUNCTION_NAME',
-        Payload: JSON.stringify({ processedCards })
-    }).promise();
+  //Get Scan
+  postData_getScan.paramIn.access_token = token;
 
-    console.log(response);
+  /* postData_getCards.paramIn.SessionInfo.WorkflowId = wid; */
 
-    return({
-        "statusCode": 200, 
-        "isBase64Encoded": false,
-        "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-        "body": JSON.stringify({result: 'OK'})
-      });
+  postData_getScan = JSON.stringify(postData_getScan);
+
+  response = await post(options_getScan, postData_getScan);
+
+  if (response == null) {
+    return ({
+      "statusCode": 200,
+      "isBase64Encoded": false,
+      "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+      "body": JSON.stringify({ result: 'KO', reason: 'Something wrong with getting scan' })
+    });
+  }
+
+  /*  if (numRecords === 0) {
+       // return the number of records and stop here
+       numRecords = response.RetrieveCardsByParamResult.HitCount;
+       return({
+           "statusCode": 200, 
+           "isBase64Encoded": false,
+           "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+           "body": JSON.stringify({result: 'OK', numRecords: numRecords})
+         });
+   }
+ 
+   let cards = response.RetrieveCardsByParamResult.Cards;
+   
+   let processedCards = [];
+   
+   cards.forEach(card => {
+     let cardId = card.CardId;
+     let indexes = card.Indexes;
+     let progressivo = card.CardProg;
+     if (indexes != null) {
+       let dataFirma = '';
+       let societaFondo = '';
+       let controparte = '';
+       let piva = '';
+       let tipoFornitore = '';
+       let numSistema = '';
+       
+       indexes.forEach(index => {
+         if (index.FieldDescription === 'Data firma' ) {
+           dataFirma = index.FieldValue;
+         } else if (index.FieldDescription === 'Società/Fondo' ) {
+           societaFondo = index.FieldValue;
+         } else if (index.FieldDescription === 'Controparte' ) {
+           controparte = index.FieldValue;
+         } else if (index.FieldDescription === 'P.IVA' ) {
+           piva = index.FieldValue;
+         } else if (index.FieldDescription === 'Tipologia fornitore' ) {
+           tipoFornitore = index.FieldValue;
+         } else if (index.FieldDescription === 'N° di sistema' ) {
+           numSistema = index.FieldValue;
+         }
+       });
+       if (tipoFornitoreFilter.indexOf(tipoFornitore) > -1) {
+         processedCards.push({"cardId": cardId, "progressivo": progressivo, "dataFirma": dataFirma, "societaFondo": societaFondo, "controparte": controparte, "piva": piva, "tipoFornitore": tipoFornitore, "numSistema": numSistema});
+       }
+     }
+   });
+   
+   console.log(processedCards); */
+
+
+  response = await lambda.invoke({
+    FunctionName: 'FUNCTION_NAME',
+    Payload: JSON.stringify({ processedCards }) //to change the payload
+  }).promise();
+
+  console.log(response);
+
+  return ({
+    "statusCode": 200,
+    "isBase64Encoded": false,
+    "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+    "body": JSON.stringify({ result: 'OK' })
+  });
 };
