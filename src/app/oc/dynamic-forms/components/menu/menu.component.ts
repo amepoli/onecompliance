5666e03677cd134c4ac555ee36a67d91fbe66b7d
deidStorm <d.tonicello95@gmail.com>
2022-06-24 18:32:42 +0200

    enh

--- src/app/oc/dynamic-forms/components/menu/menu.component.ts
+++ src/app/oc/dynamic-forms/components/menu/menu.component.ts
@@ -6,9 +6,10 @@ import { PubSubService } from 'app/oc/services';
 @Component({
   selector: "app-menu",
   template: `
-  <div align="center" [ngStyle]="{'display': 'inline-block', 'width': '100%'}">
-    <button mat-icon-button color="primary" [disabled]="field.readonly" [matMenuTriggerFor]="matMenu1"
-    [style.width]="'100%'" [style.height.px]="'64'" [style.padding]="'16px'" [style.border-radius]="'4px'" [style.background-color]="field.style && field.style.background_color? field.style.background_color: 'transparent'" [style.color]="field.style && field.style.font_color? field.style.font_color: null">
+  <div align="center" [ngStyle]="{'display': 'inline-block', 'width': '100%'}" [style.padding-top]="'15px'">
+    <button mat-raised-button color="primary" [disabled]="field.readonly" [matMenuTriggerFor]="matMenu1"
+    [style.width]="'100%'" [style.height.px]="'64'" [style.border-radius]="'4px'" [style.background-color]="'#00008B'" [style.color]="field.style && field.style.font_color? field.style.font_color: null"
+    [style.padding]="'0px'" [style.border]="'0.1px solid #144FB7'">
       {{field.label}}<mat-icon>{{field.buttonIcon}}</mat-icon>
     </button>
     <mat-menu #matMenu1="matMenu">
