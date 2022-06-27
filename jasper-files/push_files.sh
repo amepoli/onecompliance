faaaf8f48bf432f95271c6060981e836e739bf83
nikapov <nicola@sand2cloud.com>
2022-06-27 13:24:25 +0200

    Move report push from scp to sftp

--- jasper-files/push_files.sh
+++ jasper-files/push_files.sh
@@ -9,9 +9,7 @@ fi
 aws s3 cp . s3://gorico2-reports/Jasper/ --recursive --exclude "*" --include "*.jasper" --exclude "MyReports/*" 
 aws s3 cp . s3://gorico2-reports/Jasper/reports/ --recursive --exclude "*" --include "*.jrxml" --exclude "MyReports/*"
 aws s3 cp ./logos/. s3://gorico2-reports/Jasper/logos/ --recursive --exclude "*" --include "*.png"
-scp -i ./server_ec2.pem *.jasper ec2-user@18.197.167.37:/home/ec2-user/gorico_reports_docker/s3-fs/Jasper
-scp -i ./server_ec2.pem *.jrxml ec2-user@18.197.167.37:/home/ec2-user/gorico_reports_docker/s3-fs/Jasper/reports
-scp -i ./server_ec2.pem ./logos/*.png ec2-user@18.197.167.37:/home/ec2-user/gorico_reports_docker/s3-fs/Jasper/logos
+sftp -i reports.key -P 5222 -b sftp.txt reports@18.197.167.37
 cd ../dynamo-tables/reports/
 ./push_tables.sh $1
 cd -
