9791a78d2c231add2d31731eff9126a24a944364
deidStorm <d.tonicello95@gmail.com>
2022-09-29 18:41:24 +0200

    update lambda

--- lambda/zabbix/index.js
+++ lambda/zabbix/index.js
@@ -22,21 +22,41 @@ exports.handler = async (event) => {
     console.log("Company is: ", codice_azienda);
 
     const timestamp = new Date().toISOString();
+    
+    let JSON_object = JSON.stringify(event);
 
     try {
+        
         let client = await pool.connect();
 
-        let JSON_object = JSON.stringify(event);
+        /* PROCESSO IL RECORD */
 
-        const query = `insert into imports.imported_objects (codice_azienda, data_import, object) values ('${codice_azienda}', '${timestamp}', '${JSON_object}');`;
+        if (JSON_object.indexOf("\"hosts\"") != -1) {
 
-        console.log(query);
+            console.log('Hosts');
+            const query = `SELECT imports.process_object('${codice_azienda}', '${timestamp}', '${JSON_object}');`;
+            
+            console.log(query);
 
-        let reply = await client.query(query);
+            let reply = await client.query(query);
 
-        console.log(JSON.stringify(reply));
+            console.log(JSON.stringify(reply));
+
+        } else {
+
+            console.log('No hosts');
+            
+            const query = `INSERT INTO imports.imported_objects (codice_azienda, data_import, object) values ('${codice_azienda}', '${timestamp}', '${JSON_object}');`;
+
+            console.log(query);
+
+            let reply = await client.query(query);
+
+            console.log(JSON.stringify(reply));
+        }
 
         await client.end();
+
     } catch (e) {
         console.log(e);
     }
