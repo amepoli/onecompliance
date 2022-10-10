e4b369d1e1555c4287b1ab6ed7cffd690515b905
deidStorm <d.tonicello95@gmail.com>
2022-10-10 18:54:27 +0200

    delete console.log

--- lambda/regulat/index.js
+++ lambda/regulat/index.js
@@ -16,7 +16,6 @@ exports.handler = async (event, context) => {
   const checkId = queryParams['checkId'];
   const dynamoUser = queryParams['dynamoUser'];
   const connectedRegistries = JSON.parse(queryParams['connected_registries']);
-  console.log(queryParams['isLightScan']);
   const isLightScan = (queryParams['isLightScan'] === 'true') ? 'LIGHTSCAN' : '';
   let hostName;
   let scannedData = '';
