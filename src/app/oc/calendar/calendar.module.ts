7af0f03edb7ab9fc5f1bbf8ba63b53ac92e88edb
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2022-10-12 23:09:18 +0500

    Added CalendarEventDialog component

--- src/app/oc/calendar/calendar.module.ts
+++ src/app/oc/calendar/calendar.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { CalendarEventDialogModule } from '../dialogs/calendar-event.dialog/calendar-event.dialog.module';
 import { CalendarComponent } from './calendar.component';
 
 @NgModule({
@@ -9,6 +10,8 @@ import { CalendarComponent } from './calendar.component';
     imports: [
         CommonModule,
         MatButtonModule,
+
+        CalendarEventDialogModule
         // BrowserAnimationsModule,
     ],
     exports: [CalendarComponent
