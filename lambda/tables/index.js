78a3f4ebbbdf12ab3da573fa6be3c129991e78da
deidStorm <d.tonicello95@gmail.com>
2022-10-18 16:43:13 +0200

    hide log

--- lambda/tables/index.js
+++ lambda/tables/index.js
@@ -1274,7 +1274,7 @@ async function processPreMainPost(queryString, client, notFullTable, isGet) {
     // main query
     if (queryString.mainQuery != null && queryString.mainQuery !== '') {
         let query = replaceLocalKeys(queryString.mainQuery, local_keys_pre);
-        console.log('Running main query : ', query);
+        //console.log('Running main query : ', query);
         queryData = await client.query(query);
         queryData = queryData.rows;
         console.log('Main query : ', query, ' result : ', queryData);
