b1f56b91fb2bcc1ec5012eefb1bfb24c4110efca
deidStorm <d.tonicello95@gmail.com>
2022-10-27 15:48:22 +0200

    add primary key

--- lambda/calendar/index.js
+++ lambda/calendar/index.js
@@ -39,17 +39,17 @@ exports.handler = async (event) => {
 
     const company = queryParams['company'];
     const query = `
-    SELECT 'sondaggi' as object_name , id_sondaggio::varchar as object_id, titolo as titolo, descrizione as descrizione, data_prevista as data_inizio, data_esecuzione as data_fine, 'green' as event_color 
+    SELECT 'sondaggi' as object_name , ('{"codice_azienda":"'||codice_azienda||'", "id_sondaggio":"'||id_sondaggio||'"}')::jsonb as object_id, titolo as titolo, descrizione as descrizione, data_prevista as data_inizio, data_esecuzione as data_fine, 'green' as event_color 
     FROM entrasp.sondaggi 
     WHERE codice_azienda='${company}' AND data_prevista is not null 
     --AND EXTRACT (YEAR FROM data_prevista)||'-'||EXTRACT (YEAR FROM data_prevista) = SUBSTR(CURRENT_DATE::varchar,0,8)
     UNION 
-    SELECT 'compiti' as object_name , codice_compito as object_id, titolo as titolo, note as descrizione, data_segnalazione as data_inizio, data_chiusura as data_fine, 'orange' as event_color 
+    SELECT 'compiti' as object_name , ('{"codice_azienda":"'||codice_azienda||'", "codice_compito":"'||codice_compito||'"}')::jsonb as object_id, titolo as titolo, note as descrizione, data_segnalazione as data_inizio, data_chiusura as data_fine, 'orange' as event_color 
     FROM entrasp.compiti 
     WHERE codice_azienda='${company}'
     --AND EXTRACT (YEAR FROM data_segnalazione)||'-'||EXTRACT (YEAR FROM data_segnalazione) = SUBSTR(CURRENT_DATE::varchar,0,8)
     UNION
-    SELECT 'riunioni' as object_name , id_riunione::varchar as object_id, convocazione as titolo, oggetto as descrizione, data_riunione as data_inizio, data_riunione as data_fine, 'blue' as event_color 
+    SELECT 'riunioni' as object_name , ('{"codice_azienda":"'||codice_azienda||'", "id_riunione":"'||id_riunione||'"}')::jsonb as object_id, convocazione as titolo, oggetto as descrizione, data_riunione as data_inizio, data_riunione as data_fine, 'blue' as event_color 
     FROM entrasp.riunioni 
     WHERE codice_azienda='${company}'
     --AND EXTRACT (YEAR FROM data_riunione)||'-'||EXTRACT (YEAR FROM data_riunione) = SUBSTR(CURRENT_DATE::varchar,0,8)
