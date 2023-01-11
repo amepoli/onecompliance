fcfdaa001a91337aac51c1641b15f69adc9bd649
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2023-01-11 17:55:42 +0500

    Added DomChange Directive and fixed table pagination

---
+++ src/app/oc/directives/dom-change.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { DomChangeDirective } from './dom-change.directive';
+
+
+@NgModule({
+    declarations: [
+        DomChangeDirective,
+    ],
+    imports: [
+        
+    ],
+    exports: [
+        DomChangeDirective
+    ]
+})
+export class DomChangeDirectiveModule { }
