0439f52b2d9d7412b1dfd1c5ef16baf333acb89f
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2023-04-17 16:06:05 +0500

    Implemented Search menu

---
+++ src/app/oc/views/navigation-search/navigation-search.component.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+import 'rxjs/add/operator/filter';
+import { AuthService } from 'app/oc/services';
+
+@Component({
+    selector: 'navigation-search',
+    templateUrl: './navigation-search.component.html',
+    styleUrls: ['./navigation-search.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavigationSearchComponent {
+
+    curValue: string = null;
+
+    constructor(
+        private _authService: AuthService) {
+    }
+
+    filter() {
+        this._authService.filterMenu(this.curValue);
+    }
+}
