0ba4b6e3d843591bf1a5a2d99925a7e5db84573e
deidStorm <d.tonicello95@gmail.com>
2023-05-09 16:09:02 +0200

    fix

--- lambda/csv_creator/index.js
+++ lambda/csv_creator/index.js
@@ -20,8 +20,7 @@ const pool = new Pool({
 });
 
 const default_separator_out = ';';
-const bucket_name = 'BUCKET_NAME';
-const default_bucket = 'gorico2-migration';
+const default_bucket_name = 'BUCKET_NAME';
 const default_company = 'DEMO';
 const default_folder = 'test';
 const default_file_out = 'test_csv_creator.csv';
@@ -30,7 +29,7 @@ const default_query = 'SELECT 1 AS one,2 AS two,3 AS three';
 
 var csvFile = '';
 
-async function writeFileToS3(key, data, bucket = bucket_name) {
+async function writeFileToS3(key, data, bucket = default_bucket_name) {
     console.log('Writing File to s3');
     var params = {
         Bucket: bucket,
@@ -111,7 +110,7 @@ async function start(bucket = bucket, company = default_company, folder = defaul
 
 async function processQueryParams(queryParams) {
 
-    let bucket = bucket_name ? bucket_name : default_bucket; //queryParams.bucket ? queryParams.bucket : default_bucket;
+    let bucket = queryParams.bucket ? queryParams.bucket : default_bucket_name; //bucket_name ? bucket_name : default_bucket;
     let company = queryParams.company ? queryParams.company : default_company;
     let folder = queryParams.folder ? queryParams.folder : default_folder;
     let file_out = queryParams.file_out ? queryParams.file_out : default_file_out;
