8f2ca0dad168665d232419be771adfdff3ad65d4
deidStorm <d.tonicello95@gmail.com>
2023-05-09 18:50:44 +0200

    define step

--- lambda/woocommerce_VPC/index.ts
+++ lambda/woocommerce_VPC/index.ts
@@ -1,8 +1,10 @@
-exports.handler = async() => {
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify('OK')
-        };
-
+exports.handler = async () => {
+    // 1 - Query the DB to pick the checklist to send
+    // 2 - For loop in which create the object for the API
+    //      Create the file excel and save the URL
+    // 3 - Return all data
+    return {
+        statusCode: 200,
+        body: JSON.stringify('OK')
+    };
 };
