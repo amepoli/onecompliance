b8f539be9ee6c2244a2c0b835f57e90804082f40
deidStorm <d.tonicello95@gmail.com>
2023-05-10 18:09:34 +0200

    create and save the checklist

--- lambda/woocommerce_VPC/index.js
+++ lambda/woocommerce_VPC/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -34,12 +35,227 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var _this = this;
-exports.handler = function () { return __awaiter(_this, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        return [2 /*return*/, {
-                statusCode: 200,
-                body: JSON.stringify('OK')
-            }];
+exports.__esModule = true;
+var excel = require("node-excel-export");
+var AWS = require("aws-sdk");
+var Pool = require("pg-pool");
+AWS.config.update({ region: process.env.REGION });
+var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+var pool = new Pool({
+    host: process.env.HOST_NAME,
+    database: process.env.DB_NAME,
+    user: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+    max: 1,
+    min: 0,
+    idleTimeoutMillis: 300000,
+    connectionTimeoutMillis: 1000
+});
+function dataPrepare2xls(dataset, title) {
+    var styles = {
+        headerDark: {
+            fill: {
+                fgColor: {
+                    rgb: 'FF008000'
+                }
+            },
+            font: {
+                color: {
+                    rgb: 'FFFFFFFF'
+                },
+                sz: 18,
+                bold: true
+            }
+        },
+        title: {
+            fill: {
+                fgColor: {
+                    rgb: 'FF2B679D'
+                }
+            },
+            font: {
+                color: {
+                    rgb: 'FFFFFFFF'
+                },
+                sz: 34,
+                bold: true
+            }
+        },
+        data: {
+            font: {
+                color: {
+                    rgb: 'FFFFFFFF'
+                },
+                sz: 16,
+                bold: true
+            },
+            fill: {
+                fgColor: {
+                    rgb: 'FF16ACFF'
+                }
+            }
+        },
+        cellRed: {
+            fill: {
+                fgColor: {
+                    rgb: 'FFFF0000'
+                }
+            }
+        },
+        cellUnderlined: {
+            font: {
+                color: {
+                    rgb: 'FF0645AD'
+                },
+                underline: true
+            }
+        }
+    };
+    //Array of objects representing heading rows (very top)
+    var heading = [
+        [{
+                value: title,
+                style: styles.title
+            }] // <-- It can be only values
+    ];
+    //console.log('generate secondary sheets with dataset: ', dataset);
+    var specification = {};
+    var property = Object.keys(dataset.rows[0]);
+    for (var i = 0; i < property.length; i++) {
+        specification[property[i]] = {
+            displayName: property[i],
+            headerStyle: styles.data,
+            width: 120
+        };
+    }
+    var merges = [{
+            start: {
+                row: 1,
+                column: 1
+            },
+            end: {
+                row: 1,
+                column: property.length
+            }
+        }];
+    return {
+        name: title,
+        heading: heading,
+        merges: merges,
+        specification: specification,
+        data: dataset.rows // <-- Report data
+    };
+}
+exports.handler = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var queryResult, bodyResponse, d, today, nextYearFromToday, query, _loop_1, filename, s3ParamsInsert, s3ParamsUrl, _a, _b, _i, row;
+    return __generator(this, function (_c) {
+        switch (_c.label) {
+            case 0:
+                bodyResponse = { result: 'Ok', reason: null, response: [] };
+                d = new Date();
+                today = d.toISOString();
+                d.setFullYear(d.getFullYear() + 1);
+                nextYearFromToday = d.toISOString();
+                query = "SELECT 'select coalesce(id_sezione,1) as sezione, ordinamento, descrizione as domanda, note as note_domanda, entrasp.risposte_previste_mostra1(codice_azienda, id_modello_test, id_modello_test_vr, id_domanda) as risposte_previste from entrasp.domande where codice_azienda=''SITO'' and id_modello_test='||mt.id_modello_test||' and id_modello_test_vr='||mtvr.id_modello_test_vr||' order by id_sezione asc, ordinamento asc' query_excel_to_create,\n                    mt.titolo AS name,\n                    concat_ws('.',mt.id_modello_test,mtvr.id_modello_test_vr,mt.codice) AS sku,\n                    concat_ws('.',mt.id_modello_test,mtvr.id_modello_test_vr,mt.codice) AS partnersku,\n                    '0' AS ean,\n                    mt.descrizione AS description,\n                    mt.titolo AS shortdescription,\n                    mt.descrizione AS descriptionit,\n                    mt.descrizione AS descriptionen,\n                    CURRENT_TIMESTAMP AS dateonsalefrom,\n                    CURRENT_TIMESTAMP+interval '1 year' AS dateonsaleto,\n                    '999' AS stock,\n                    '49.99' AS price,\n                    NULL AS downloads,\n                    '-1' AS downloadlimit,\n                    '-1' AS downloadexpiry\n                FROM entrasp.modelli_test mt\n                INNER JOIN entrasp.modelli_test_vr mtvr\n                    ON mt.codice_azienda=mtvr.codice_azienda\n                    AND mt.id_modello_test=mtvr.id_modello_test\n                WHERE mtvr.id_modello_test_vr=entrasp.grc_max_id_mdt_vr(mt.codice_azienda, mt.id_modello_test)\n                    AND mt.codice_azienda='SITO'\n                    AND mtvr.data_ins::date = CURRENT_DATE;";
+                return [4 /*yield*/, pool
+                        .query(query)
+                        .then(function (res) { return queryResult = res.rows.length > 0 ? res : null; })["catch"](function (err) {
+                        console.error("Error executing query \"" + query + "\"", err.stack);
+                        bodyResponse = { result: 'KO', reason: 'Something went wrong quering the DB', response: null };
+                    })];
+            case 1:
+                _c.sent();
+                if (!queryResult) return [3 /*break*/, 6];
+                _loop_1 = function (row) {
+                    var checkListTitle, query_excel_to_create, excelData, excelFile, url;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                checkListTitle = queryResult.rows[row].name;
+                                query_excel_to_create = queryResult.rows[row].query_excel_to_create;
+                                excelData = [];
+                                if (!query_excel_to_create) return [3 /*break*/, 2];
+                                // here it prepares secondary sheets to the main one
+                                return [4 /*yield*/, pool
+                                        .query(query_excel_to_create)
+                                        .then(function (res) { return excelData.push(dataPrepare2xls(res, checkListTitle)); })["catch"](function (err) {
+                                        console.error("Error executing query \"" + query_excel_to_create + "\"", err.stack);
+                                        bodyResponse = { result: 'KO', reason: 'Something went wrong quering the DB', response: null };
+                                    })];
+                            case 1:
+                                // here it prepares secondary sheets to the main one
+                                _a.sent();
+                                _a.label = 2;
+                            case 2:
+                                excelFile = excel.buildExport(excelData);
+                                // configurations to upload the file on S3
+                                filename = checkListTitle + '.xlsx'; // generate a 'unique' identifier as filename
+                                s3ParamsInsert = {
+                                    Bucket: process.env.BUCKET_NAME,
+                                    Key: 'test/' + filename,
+                                    Body: excelFile
+                                };
+                                s3ParamsUrl = {
+                                    Bucket: process.env.BUCKET_NAME,
+                                    Key: 'test/' + filename
+                                };
+                                // upload to S3
+                                return [4 /*yield*/, s3.putObject(s3ParamsInsert).promise()];
+                            case 3:
+                                // upload to S3
+                                _a.sent();
+                                url = s3.getSignedUrl('getObject', s3ParamsUrl);
+                                //push the check list
+                                bodyResponse.response.push({
+                                    name: queryResult.rows[row].name,
+                                    sku: queryResult.rows[row].sku,
+                                    partnerSku: queryResult.rows[row].partnersku,
+                                    ean: queryResult.rows[row].ean,
+                                    description: queryResult.rows[row].description,
+                                    shortDescription: queryResult.rows[row].shortdescription,
+                                    descriptionIt: queryResult.rows[row].descriptionit,
+                                    descriptionEn: queryResult.rows[row].descriptionen,
+                                    dateOnSaleFrom: today,
+                                    dateOnSaleTo: nextYearFromToday,
+                                    stock: queryResult.rows[row].stock,
+                                    price: queryResult.rows[row].price,
+                                    downloads: [{
+                                            id: filename,
+                                            name: filename,
+                                            file: url
+                                        }],
+                                    downloadLimit: queryResult.rows[row].downloadlimit,
+                                    downloadExpiry: queryResult.rows[row].downloadexpiry
+                                });
+                                return [2 /*return*/];
+                        }
+                    });
+                };
+                _a = [];
+                for (_b in queryResult.rows)
+                    _a.push(_b);
+                _i = 0;
+                _c.label = 2;
+            case 2:
+                if (!(_i < _a.length)) return [3 /*break*/, 5];
+                row = _a[_i];
+                return [5 /*yield**/, _loop_1(row)];
+            case 3:
+                _c.sent();
+                _c.label = 4;
+            case 4:
+                _i++;
+                return [3 /*break*/, 2];
+            case 5:
+                ;
+                _c.label = 6;
+            case 6:
+                console.log('bodyResponse: ', JSON.stringify(bodyResponse));
+                return [2 /*return*/, {
+                        statusCode: 200,
+                        body: JSON.stringify(bodyResponse)
+                    }];
+        }
     });
 }); };
--- lambda/woocommerce_VPC/index.ts
+++ lambda/woocommerce_VPC/index.ts
@@ -1,10 +1,241 @@
+import * as excel from 'node-excel-export';
+import * as AWS from 'aws-sdk';
+import * as Pool from 'pg-pool';
+
+AWS.config.update({ region: process.env.REGION });
+
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+const pool = new Pool({
+    host: process.env.HOST_NAME,
+    database: process.env.DB_NAME,
+    user: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+    max: 1,
+    min: 0,
+    idleTimeoutMillis: 300000,
+    connectionTimeoutMillis: 1000
+});
+
+function dataPrepare2xls(dataset, title) {
+
+    const styles = {
+        headerDark: {
+            fill: {
+                fgColor: {
+                    rgb: 'FF008000'
+                },
+
+            },
+            font: {
+                color: {
+                    rgb: 'FFFFFFFF'
+                },
+                sz: 18,
+                bold: true
+            }
+        },
+        title: {
+            fill: {
+                fgColor: {
+                    rgb: 'FF2B679D'
+                },
+            },
+            font: {
+                color: {
+                    rgb: 'FFFFFFFF'
+                },
+                sz: 34,
+                bold: true
+            }
+        },
+        data: {
+            font: {
+                color: {
+                    rgb: 'FFFFFFFF'
+                },
+                sz: 16,
+                bold: true
+            },
+            fill: {
+                fgColor: {
+                    rgb: 'FF16ACFF'
+                },
+            }
+        },
+        cellRed: {
+            fill: {
+                fgColor: {
+                    rgb: 'FFFF0000'
+                }
+            }
+        },
+        cellUnderlined: {
+            font: {
+                color: {
+                    rgb: 'FF0645AD'
+                },
+                underline: true
+            }
+        }
+    };
+
+    //Array of objects representing heading rows (very top)
+    const heading = [
+        [{
+            value: title,
+            style: styles.title
+        }] // <-- It can be only values
+    ];
+
+    //console.log('generate secondary sheets with dataset: ', dataset);
+    const specification = {};
+
+    let property = Object.keys(dataset.rows[0]);
+    for (let i = 0; i < property.length; i++) {
+        specification[property[i]] = {
+            displayName: property[i],
+            headerStyle: styles.data,
+            width: 120
+        };
+    }
+
+    const merges = [{
+        start: {
+            row: 1,
+            column: 1
+        },
+        end: {
+            row: 1,
+            column: property.length
+        }
+    }];
+
+    return {
+        name: title, // <- Specify sheet name (optional)
+        heading: heading, // <- Raw heading array (optional)
+        merges: merges, // <- Merge cell ranges
+        specification: specification, // <- Report specification
+        data: dataset.rows // <-- Report data
+    }
+}
+
+
 exports.handler = async () => {
     // 1 - Query the DB to pick the checklist to send
-    // 2 - For loop in which create the object for the API
-    //      Create the file excel and save the URL
-    // 3 - Return all data
+    let queryResult;
+    let bodyResponse = { result: 'Ok', reason: null, response: [] };
+    let d = new Date();
+    const today = d.toISOString();
+    d.setFullYear(d.getFullYear() + 1);
+    const nextYearFromToday= d.toISOString();
+
+    let query = `SELECT 'select coalesce(id_sezione,1) as sezione, ordinamento, descrizione as domanda, note as note_domanda, entrasp.risposte_previste_mostra1(codice_azienda, id_modello_test, id_modello_test_vr, id_domanda) as risposte_previste from entrasp.domande where codice_azienda=''SITO'' and id_modello_test='||mt.id_modello_test||' and id_modello_test_vr='||mtvr.id_modello_test_vr||' order by id_sezione asc, ordinamento asc' query_excel_to_create,
+                    mt.titolo AS name,
+                    concat_ws('.',mt.id_modello_test,mtvr.id_modello_test_vr,mt.codice) AS sku,
+                    concat_ws('.',mt.id_modello_test,mtvr.id_modello_test_vr,mt.codice) AS partnersku,
+                    '0' AS ean,
+                    mt.descrizione AS description,
+                    mt.titolo AS shortdescription,
+                    mt.descrizione AS descriptionit,
+                    mt.descrizione AS descriptionen,
+                    CURRENT_TIMESTAMP AS dateonsalefrom,
+                    CURRENT_TIMESTAMP+interval '1 year' AS dateonsaleto,
+                    '999' AS stock,
+                    '49.99' AS price,
+                    NULL AS downloads,
+                    '-1' AS downloadlimit,
+                    '-1' AS downloadexpiry
+                FROM entrasp.modelli_test mt
+                INNER JOIN entrasp.modelli_test_vr mtvr
+                    ON mt.codice_azienda=mtvr.codice_azienda
+                    AND mt.id_modello_test=mtvr.id_modello_test
+                WHERE mtvr.id_modello_test_vr=entrasp.grc_max_id_mdt_vr(mt.codice_azienda, mt.id_modello_test)
+                    AND mt.codice_azienda='SITO'
+                    AND mtvr.data_ins::date = CURRENT_DATE;`;
+
+    await pool
+        .query(query)
+        .then((res: any) => queryResult = res.rows.length > 0 ? res : null)
+        .catch((err: Error) => {
+            console.error(`Error executing query "${query}"`, err.stack);
+            bodyResponse = { result: 'KO', reason: 'Something went wrong quering the DB', response: null }
+        });
+
+    if (queryResult) {
+
+        for (const row in queryResult.rows) {
+
+            let checkListTitle = queryResult.rows[row].name;
+            let query_excel_to_create = queryResult.rows[row].query_excel_to_create;
+
+            // generate report
+
+            let excelData = [];
+
+            if (query_excel_to_create) {
+                // here it prepares secondary sheets to the main one
+                await pool
+                    .query(query_excel_to_create)
+                    .then((res: any) => excelData.push(dataPrepare2xls(res, checkListTitle)))
+                    .catch((err: Error) => {
+                        console.error(`Error executing query "${query_excel_to_create}"`, err.stack);
+                        bodyResponse = { result: 'KO', reason: 'Something went wrong quering the DB', response: null }
+                    });
+            }
+
+            // generate the file
+            let excelFile = excel.buildExport(excelData);
+
+            // configurations to upload the file on S3
+            var filename = checkListTitle + '.xlsx'; // generate a 'unique' identifier as filename
+
+            var s3ParamsInsert = {
+                Bucket: process.env.BUCKET_NAME,
+                Key: 'test/' + filename,
+                Body: excelFile
+            };
+            var s3ParamsUrl = {
+                Bucket: process.env.BUCKET_NAME,
+                Key: 'test/' + filename
+            };
+
+            // upload to S3
+            await s3.putObject(s3ParamsInsert).promise();
+
+            // get the uploaded file url
+            let url = s3.getSignedUrl('getObject', s3ParamsUrl);
+
+            //push the check list
+            bodyResponse.response.push({
+                name: queryResult.rows[row].name,
+                sku: queryResult.rows[row].sku,
+                partnerSku: queryResult.rows[row].partnersku,
+                ean: queryResult.rows[row].ean,
+                description: queryResult.rows[row].description,
+                shortDescription: queryResult.rows[row].shortdescription,
+                descriptionIt: queryResult.rows[row].descriptionit,
+                descriptionEn: queryResult.rows[row].descriptionen,
+                dateOnSaleFrom: today,
+                dateOnSaleTo: nextYearFromToday,
+                stock: queryResult.rows[row].stock,
+                price: queryResult.rows[row].price,
+                downloads: [{
+                    id: filename,
+                    name: filename,
+                    file: url
+                }],
+                downloadLimit: queryResult.rows[row].downloadlimit,
+                downloadExpiry: queryResult.rows[row].downloadexpiry
+            });
+        };
+    }
+
+    console.log('bodyResponse: ',JSON.stringify(bodyResponse));
+
     return {
         statusCode: 200,
-        body: JSON.stringify('OK')
+        body: JSON.stringify(bodyResponse)
     };
 };
