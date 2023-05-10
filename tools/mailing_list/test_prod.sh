40f3e55cde38b5068b4d88d0cebbc6985e30750d
deidStorm <d.tonicello95@gmail.com>
2023-05-10 14:55:00 +0200

    mailing list test in prod

---
+++ tools/mailing_list/test_prod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+aws lambda invoke \
+--function-name mailing_list_prod \
+--cli-binary-format raw-in-base64-out \
+--payload  '{"mode": "test"}' \
+mailing_list.json
+
+mailingList=`cat ./mailing_list.json`
+#echo $mailingList
+
+for row in $(echo "${mailingList}" | jq -r '.[] | @base64'); do
+_jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+    #echo $(_jq)
+    aws lambda invoke \
+    --function-name email_trigger_prod \
+    --cli-binary-format raw-in-base64-out \
+    --payload "$(_jq)" \
+    result.json
+done
+
+#Stoiricizzo i valori sul database (commentato poichè psql non installato su questo sistema)
+#psql -h onecompliance-aurora.caxbbckt9xen.eu-central-1.rds.amazonaws.com -U postgres -d Gorico -c "select entrasp.insert_val_ind_report_mail();"
