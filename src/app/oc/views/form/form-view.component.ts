219a6d8adcf016c6d00a86ee666ee1ec4a6a2a7e
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2023-07-19 15:17:09 +0500

    Updated custom dialog to add hiding the save button

--- src/app/oc/views/form/form-view.component.ts
+++ src/app/oc/views/form/form-view.component.ts
@@ -25,7 +25,8 @@ export class FormViewComponent implements OnChanges, OnInit, OnDestroy {
     
     // Is form-view part of dialog
     @Input() isDialog: boolean = false;
-
+    @Input() showSaveButton: boolean = true;
+    
     // Keys that are provided by external source and are passed to the values in onSave function
     @Input () externalKeys: object = {};
 
