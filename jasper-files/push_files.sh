c7043d14f29c7b08dda00336c57c0fc5b1b371f5
deidStorm <d.tonicello95@gmail.com>
2023-08-11 17:49:21 +0200

    fix script

--- jasper-files/push_files.sh
+++ jasper-files/push_files.sh
@@ -24,7 +24,7 @@ fi
 aws s3 cp . s3://gorico2-reports/Jasper/ --recursive --exclude "*" --include "*.jasper" --exclude "MyReports/*" 
 aws s3 cp . s3://gorico2-reports/Jasper/reports/ --recursive --exclude "*" --include "*.jrxml" --exclude "MyReports/*"
 aws s3 cp ./logos/. s3://gorico2-reports/Jasper/logos/ --recursive --exclude "*" --include "*.png"
-sftp -i reports.key -P 5222 -b sftp.txt reports@"$HOSTNAME"
+sftp -i reports.key -o "StrictHostKeyChecking=no" -P 5222 -b sftp.txt reports@"$HOSTNAME"
 cd ../dynamo-tables/reports/
 ./push_tables.sh $1
 cd -
