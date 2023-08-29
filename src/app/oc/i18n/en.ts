cf972cfda218e1eb5fd319cb0131bb4fbee77b98
marcoguad <marco.guadagnini@auditft.it>
2023-08-29 10:25:51 +0200

    trnaslate

--- src/app/oc/i18n/en.ts
+++ src/app/oc/i18n/en.ts
@@ -3,9 +3,21 @@ export const locale = {
     data: {
         'RESOURCES': {
             'Utente': 'User',
-            'hello': 'Hello',
             'office': 'Office',
-            'creation_date': 'Creation date'
+            'creation_date': 'Creation date',
+            'report_date': 'Report Date',
+            'state': 'State',
+            'id_responsible': 'ID Responsible',
+            'customer_fund':'Customer - Fund',
+            'reporting_office': 'Reporting Office',
+            'monitoring_office': 'Monitoring Office',
+            'data_breach_code': 'Data Breach Code',
+            'data_breach_code_parent': 'Data Breach Code Parent',
+            'closing_date': 'Closing Date',
+            'reference_date': 'Reference Date',
+            'title':'Title',
+            'notes': 'Notes',
+            'actions_updates':'Actions/Updates'
         }
     }
 };
--- src/app/oc/i18n/it.ts
+++ src/app/oc/i18n/it.ts
@@ -2,10 +2,22 @@ export const locale = {
     lang: 'it',
     data: {
         'RESOURCES': {
-            'Utente': 'Utente',
-            'hello': 'Ciao',
+            'user': 'Utente',
             'office': 'Centro Gestionale',
-            'creation_date': 'Data creazione'
+            'creation_date': 'Data creazione',
+            'report_date': "Data Segnalazione",
+            'state':'Stato',
+            'id_responsible': 'ID Responsabile',
+            'customer_fund': 'Cliente - Fondo',
+            'reporting_office': 'Centro Gestionale Segnalante',
+            'monitoring_office': 'Centro Gestionale di Monitoraggio',
+            'data_breach_code': 'Codice Data Breach',
+            'data_breach_code_parent': 'Codice Data Breach Parent',
+            'closing_date':'Data Chiusura',
+            'reference_date': 'Data Competenza',
+            'title': 'Titolo',
+            'notes': 'Note',
+            'actions_updates': 'Azioni/Aggiornamenti'
         }
     }
 };
