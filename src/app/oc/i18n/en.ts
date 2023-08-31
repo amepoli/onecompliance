304cf114505ba3b36fc917fe5a0ba8dca8d8ce4c
amepoli <amedeo.poli@auditft.it>
2023-08-31 17:40:30 +0200

    alphabetical orde

--- src/app/oc/i18n/en.ts
+++ src/app/oc/i18n/en.ts
@@ -2,7 +2,6 @@ export const locale = {
     lang: 'en',
     data: {
         'RESOURCES': {
-<<<<<<< HEAD
             actions_updates: 'Actions/Updates',
             closing_date: 'Closing Date',
             code: 'Code',
@@ -22,28 +21,7 @@ export const locale = {
             title: 'Title',
             type: 'Type',
             Utente: 'User',
-=======
-            'Utente': 'User',
-            'office': 'Office',
-            'creation_date': 'Creation date',
-            'report_date': 'Report Date',
-            'state': 'State',
-            'id_responsible': 'ID Responsible',
-            'customer_fund':'Customer - Fund',
-            'reporting_office': 'Reporting Office',
-            'monitoring_office': 'Monitoring Office',
-            'data_breach_code': 'Data Breach Code',
-            'data_breach_code_parent': 'Data Breach Code Parent',
-            'closing_date': 'Closing Date',
-            'reference_date': 'Reference Date',
-            'title':'Title',
-            'notes': 'Notes',
-            'actions_updates':'Actions/Updates',
-            'type': 'Type',
-            'code': 'Code',
-            'main_only':'Main Only',
-            'void': ''
->>>>>>> bd92945c7886e779202598d6762d10ec234ec7f9
+            void: ''
         }
     }
 };
