eb9090668a2333db5db45c2273d87b46b6850fb0
amepoli <amedeo.poli@auditft.it>
2024-07-16 13:03:20 +0200

    translate

--- python/aggiungi_translate_directory.py
+++ python/aggiungi_translate_directory.py
@@ -13,16 +13,19 @@ def aggiungi_translate(file_path, resource_prefix):
 
     added_elements = []  # Lista per tracciare gli elementi aggiunti
 
-    # Processa sia 'form_keys' sia 'table_keys'
-    for key in ['form_keys', 'table_keys','addElementSettings']:
+    # Processa sia 'form_keys' sia 'table_keys' sia 'addElementSettings'
+    for key in ['form_keys', 'table_keys', 'addElementSettings','']:
         if key in data:
             # Itera su ogni elemento nella lista associata alla chiave
             for element in data[key]:
-                # Controlla se l'elemento ha una chiave 'key' e non ha già una chiave 'translate'
-                if 'key' in element and 'translate' not in element:
-                    # Aggiunge la chiave 'translate'
-                    element['translate'] = resource_prefix + element['key']
-                    added_elements.append(element['key'])  # Aggiunge la chiave alla lista degli elementi aggiunti
+                # Controlla se l'elemento ha una chiave 'key' o 'label' e non ha già una chiave 'translate'
+                if 'translate' not in element:
+                    if 'key' in element:
+                        element['translate'] = resource_prefix + element['key']
+                        added_elements.append(element['key'])  # Aggiunge la chiave alla lista degli elementi aggiunti
+                    elif 'label' in element:
+                        element['translate'] = resource_prefix + element['label'].replace(' ', '_').lower()
+                        added_elements.append(element['label'])  # Aggiunge la chiave alla lista degli elementi aggiunti
 
     # Scrive i cambiamenti nel file JSON
     with open(file_path, 'w', encoding='utf-8') as file:
@@ -31,7 +34,7 @@ def aggiungi_translate(file_path, resource_prefix):
     return added_elements
 
 # Funzione per aggiungere la chiave 'translate' a tutti i file JSON in una directory specificata e controllare le chiavi aggiunte
-def aggiungi_translate_directory_e_controlla(directory_path, resource_prefix="RESOURCES."):
+def aggiungi_translate_directory_e_controlla(directory_path, it_ts_path, missing_elements_path, resource_prefix="RESOURCES."):
     added_elements_total = []
 
     for filename in os.listdir(directory_path):
@@ -41,17 +44,22 @@ def aggiungi_translate_directory_e_controlla(directory_path, resource_prefix="RE
             added_elements_total.extend(added_elements)
 
     # Legge il file it.ts per ottenere tutte le traduzioni esistenti
-    with open('/home/alpoli/Developement/onecompliance/src/app/oc/i18n/it.ts', 'r', encoding='utf-8') as it_file:  # !!Sostituisci con il percorso reale del file it.ts!!
+    with open(it_ts_path, 'r', encoding='utf-8') as it_file:  # !!Sostituisci con il percorso reale del file it.ts!!
         it_data = it_file.read()
 
     # Controlla gli elementi aggiunti che non esistono nel file it.ts
     non_existing_elements = [el for el in added_elements_total if resource_prefix + el not in it_data]
 
-    print("Elementi aggiunti che non esistono in it.ts:")
-    for element in non_existing_elements:
-        print(element)
+    # Scrive gli elementi mancanti in un file di testo
+    with open(missing_elements_path, 'w', encoding='utf-8') as missing_file:
+        for element in non_existing_elements:
+            missing_file.write(element + '\n')
+
+    print(f"Elementi mancanti salvati in {missing_elements_path}")
 
 # Usare un percorso ipotetico per l'esempio
-directory_path = '/home/alpoli/Developement/onecompliance/dynamo-tables/views' # !! Sostituisci con il percorso reale della tua directory!!
+directory_path = '/home/apoli/Development/onecompliance/dynamo-tables/views'  # !! Sostituisci con il percorso reale della tua directory!!
+it_ts_path = '/home/apoli/Development/onecompliance/src/app/oc/i18n/it.ts'  # !! Sostituisci con il percorso reale del file it.ts!!
+missing_elements_path = '/home/apoli/Development/onecompliance/python/file_new_translate'  # !! Sostituisci con il percorso reale del file di output!!
 
-aggiungi_translate_directory_e_controlla(directory_path)
+aggiungi_translate_directory_e_controlla(directory_path, it_ts_path, missing_elements_path)
