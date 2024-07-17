a7676e51c6d936599de4636bba594cca388146a0
edoonga <edoardo.ongaro@auditft.it>
2024-07-17 12:36:34 +0200

    traduzioni

--- src/app/oc/i18n/it.ts
+++ src/app/oc/i18n/it.ts
@@ -531,6 +531,7 @@ export const locale = {
             domande_per_ai: "Input per AI",
             domande_sub_of_presidi: "Domande Associate al Presidio",
             domandelabel: "Domanda",
+            domade_sezioni: "Domande - Sezioni",
             doppio_tag: "Doppio Tag",
             dow: "Giorno",
             dt_fine: "Data Fine legame",
