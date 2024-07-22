68123aaed314529ef87cfcd52b97775682343d2c
Jurgen Ametaj <mjgen0@gmail.com>
2024-07-22 11:31:12 +0200

    add stage changes

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,16 @@ if [ $# -eq 0 ]
     exit 0
 fi
 
+
+if [ $1 != "gorico" ] && [ $1 != "gorico_stage" ]; then
+    echo "Invalid target application. Please provide 'gorico' or 'gorico_stage'"
+    exit 1
+fi
+
 #deploy only for gorico (prod)
 if [ $1 == "gorico" ]; then
     if [ ! -f ${1}_prod.json ]; then
-        echo "Target not found!"
+        echo "$1 not found!"
         exit 0
     fi
     #re-deploy dynamo-tables
@@ -18,11 +24,31 @@ if [ $1 == "gorico" ]; then
     cd ..
 fi
 
+
+#deploy only for gorico_stage
+if [ $1 == "gorico_stage" ]; then
+    if [ ! -f ${1}.json ]; then
+        echo "$1 not found!"
+        exit 0
+    fi
+    #re-deploy dynamo-tables
+    cd dynamo-tables
+    ./deploy_tables.sh $1
+    cd ..
+fi
+
 #remove previous buils
 rm -rf dist/*
 
 #compile application
-npm run build-prod
+if [ $1 == "gorico" ]; then
+    npm run build-prod
+fi
+
+if [ $1 == "gorico_stage" ]; then
+    npm run build-stage
+fi
+
 #delete current distribution
 aws s3 rm s3://gorico2-cdk.cloud/$1 --recursive
 #upload files
