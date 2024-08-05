f10737681050d684c3f510d7c1761951cc48f772
amepoli <amedeo.poli@auditft.it>
2024-08-05 13:03:45 +0200

    incongruenze tramite python

--- python/incongruenza_entryKey.py
+++ python/incongruenza_entryKey.py
@@ -21,7 +21,7 @@ def find_mismatched_json_keys(directory_path):
                         print(f"Errore nella decodifica del file JSON: {file_path}")
 
 # Sostituisci 'your_directory_path' con il percorso effettivo della tua directory
-directory_path = '/home/apoli/Development/onecompliance/dynamo-tables/profiles'
+directory_path = '/home/apoli/Development/onecompliance/dynamo-tables/views'
 find_mismatched_json_keys(directory_path)
 
 
