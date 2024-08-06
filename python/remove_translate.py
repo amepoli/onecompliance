4fc6daf690fd9f0020bbee52570724130abf8aad
amepoli <amedeo.poli@auditft.it>
2024-08-06 12:04:42 +0200

    added field sondaggi (hidden)

--- python/remove_translate.py
+++ python/remove_translate.py
@@ -37,5 +37,5 @@ def main(input_file):
     print(f"Proprietà 'translate' rimosse: {len(removed_properties)}")
 
 if __name__ == "__main__":
-    input_file = '/home/apoli/Development/onecompliance/dynamo-tables/views/oneKYC.json'  # Inserisci il path del file di input
+    input_file = '/home/apoli/Development/onecompliance/dynamo-tables/views/contracts_investment.json'  # Inserisci il path del file di input
     main(input_file)
