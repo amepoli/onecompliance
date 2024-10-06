b5622784559aa2e57d251a301ecc7e0a9154f99d
amepoli <amedeo.poli@auditft.it>
2024-10-06 10:38:12 +0200

    new prompt

---
+++ sql/prompt_per_analisi_recredit.sql
@@ -0,0 +1,9 @@
+
+
+
+
+SELECT * FROM get_db_structure(
+    'oencompliance',                         -- Database
+    'entrasp',        -- Schemi
+    'crediti_ceduti,  pagamenti_crediti, pagamenti_fascicoli, pagamenti_tranche, mandati, fascicoli' 
+);
