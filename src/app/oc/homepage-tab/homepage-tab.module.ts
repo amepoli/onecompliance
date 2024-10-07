af4da8673216a062c5dbc8d29832de2adee491f6
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2024-10-07 19:25:55 +0100

    #823 add OCToolTip Directive in the application

--- src/app/oc/homepage-tab/homepage-tab.module.ts
+++ src/app/oc/homepage-tab/homepage-tab.module.ts
@@ -1,57 +1,57 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSortModule } from '@angular/material/sort';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { HomepageTabComponent } from './homepage-tab.component';
-import { TableViewModule } from 'app/oc/views/table/table-view.module';
-import { MatCardModule as MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
-import { FormViewModule } from '../views/form/form-view.module';
-import { FormViewToolbarModule } from '../views/form-view-toolbar/form-view-toolbar.module';
-import { BottomTabsModule } from 'app/oc/bottom-tabs/bottom-tabs.module';
-import { ImportDialogModule } from '../dialogs/import.dialog/import.dialog.module';
-import { ToolbarElementsModule } from '../toolbar-elements/toolbar-elements.module';
-import { OneCompliancePipesModule } from '../pipes/pipes.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatProgressSpinnerModule as MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSortModule } from "@angular/material/sort";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { HomepageTabComponent } from "./homepage-tab.component";
+import { TableViewModule } from "app/oc/views/table/table-view.module";
+import { MatCardModule as MatCardModule } from "@angular/material/card";
+import { RouterModule } from "@angular/router";
+import { FormViewModule } from "../views/form/form-view.module";
+import { FormViewToolbarModule } from "../views/form-view-toolbar/form-view-toolbar.module";
+import { BottomTabsModule } from "app/oc/bottom-tabs/bottom-tabs.module";
+import { ImportDialogModule } from "../dialogs/import.dialog/import.dialog.module";
+import { ToolbarElementsModule } from "../toolbar-elements/toolbar-elements.module";
+import { OneCompliancePipesModule } from "../pipes/pipes.module";
+import { OneComplianceDirectivesModule } from "../directives/directives.module";
 
 const routes = [
-  {
-    path: 'oc/homepage-tab',
-    component: HomepageTabComponent
-  }
+    {
+        path: "oc/homepage-tab",
+        component: HomepageTabComponent,
+    },
 ];
 
 @NgModule({
-  declarations: [HomepageTabComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    MatProgressSpinnerModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    TableViewModule,
-    FormViewModule,
-    FormViewToolbarModule,
-    BottomTabsModule,
-    ImportDialogModule,
-    OneCompliancePipesModule,
-    ToolbarElementsModule
-  ],
-  exports: [
-    CommonModule,
-    MatProgressSpinnerModule,
-    MatSortModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    TableViewModule,
-    FormViewModule,
-    FormViewToolbarModule,
-    HomepageTabComponent,
-  ],
-
+    declarations: [HomepageTabComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        MatProgressSpinnerModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        TableViewModule,
+        FormViewModule,
+        FormViewToolbarModule,
+        BottomTabsModule,
+        ImportDialogModule,
+        OneCompliancePipesModule,
+        ToolbarElementsModule,
+        OneComplianceDirectivesModule,
+    ],
+    exports: [
+        CommonModule,
+        MatProgressSpinnerModule,
+        MatSortModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        TableViewModule,
+        FormViewModule,
+        FormViewToolbarModule,
+        HomepageTabComponent,
+    ],
 })
-
-export class HomepageTabModule { }
+export class HomepageTabModule {}
--- src/app/oc/widgets/multi-attachments/multi-attachments.module.ts
+++ src/app/oc/widgets/multi-attachments/multi-attachments.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule as MatCardModule } from '@angular/material/card';
-import { MatOptionModule as MatOptionModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule as MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule as MatInputModule } from '@angular/material/input';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule as MatSelectModule } from '@angular/material/select';
-import { MatTabsModule as MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MultiAttachmentsComponent } from './multi-attachments.component';
-import { MatProgressBarModule as MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatTooltipModule as MatTooltipModule } from '@angular/material/tooltip';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule as MatCardModule } from "@angular/material/card";
+import { MatOptionModule as MatOptionModule } from "@angular/material/core";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule as MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule as MatInputModule } from "@angular/material/input";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatProgressSpinnerModule as MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule as MatSelectModule } from "@angular/material/select";
+import { MatTabsModule as MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MultiAttachmentsComponent } from "./multi-attachments.component";
+import { MatProgressBarModule as MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatTooltipModule as MatTooltipModule } from "@angular/material/tooltip";
+import { OneComplianceDirectivesModule } from "app/oc/directives/directives.module";
 
 @NgModule({
     imports: [
@@ -37,12 +38,10 @@ import { MatTooltipModule as MatTooltipModule } from '@angular/material/tooltip'
         MatProgressBarModule,
         MatBadgeModule,
         MatDialogModule,
-        MatTooltipModule
+        MatTooltipModule,
+        OneComplianceDirectivesModule,
     ],
-    exports: [
-        MultiAttachmentsComponent,
-    ],
-    declarations: [MultiAttachmentsComponent]
+    exports: [MultiAttachmentsComponent],
+    declarations: [MultiAttachmentsComponent],
 })
-
-export class MultiAttachmentsModule { }
+export class MultiAttachmentsModule {}
