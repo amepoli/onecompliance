76feba6684225ef4110ca64b5d9e7620ea9657fa
TMGian <www.gianmarco@hotmail.it>
2024-10-08 10:03:17 +0200

    remove_translate_json update

--- python/remove_translate_json.py
+++ python/remove_translate_json.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def remove_translate_properties(json_data):
     if isinstance(json_data, dict):
@@ -23,19 +24,38 @@ def remove_translate_properties(json_data):
     else:
         return json_data, []
 
-def main(input_file):
-    with open(input_file, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    
-    cleaned_data, removed_properties = remove_translate_properties(data)
-    
-    with open(input_file, 'w', encoding='utf-8') as file:
-        json.dump(cleaned_data, file, ensure_ascii=False, indent=4)
-    
-    for prop, label in removed_properties:
-        print(f"Rimossa: {prop} con etichetta: {label}")
-    print(f"Proprietà 'translate' rimosse: {len(removed_properties)}")
+def main():
+    # Usa la directory home dell'utente per costruire percorsi file
+    home_dir = os.path.expanduser('~')
+
+    # Definisci i percorsi relativi alla directory home
+    views_path = os.path.join(home_dir, 'Development/onecompliance/dynamo-tables/views')
+
+    # Chiedere all'utente di inserire il nome del file
+    file_name = input("Inserisci il nome del file JSON di input (es. 'file.json'): ")
+
+    # Costruire il percorso assoluto del file
+    input_file = os.path.join(views_path, file_name)
+
+    try:
+        with open(input_file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        cleaned_data, removed_properties = remove_translate_properties(data)
+
+        # Scrivere i dati puliti nello stesso file
+        with open(input_file, 'w', encoding='utf-8') as file:
+            json.dump(cleaned_data, file, ensure_ascii=False, indent=4)
+
+        for prop, label in removed_properties:
+            print(f"Rimossa: {prop} con etichetta: {label}")
+        print(f"Proprietà 'translate' rimosse: {len(removed_properties)}")
+    except FileNotFoundError:
+        print(f"Errore: il file '{input_file}' non è stato trovato.")
+    except json.JSONDecodeError:
+        print(f"Errore: il file '{input_file}' non contiene un JSON valido.")
+    except Exception as e:
+        print(f"Errore inatteso: {e}")
 
 if __name__ == "__main__":
-    input_file = '/home/apoli/Development/onecompliance/dynamo-tables/views/dialog_crediti_ceduti_dt_pagamento.json'  # Inserisci il path del file di input
-    main(input_file)
+    main()
