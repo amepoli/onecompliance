1c1f5ed0b43f6aec2b6592d7273f80512804a78a
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2024-10-11 18:26:38 +0100

    #823 updated logic for speed up of forms

--- src/app/oc/directives/dynamic-field.directive.ts
+++ src/app/oc/directives/dynamic-field.directive.ts
@@ -1,37 +1,9 @@
 import { Directive, Input, OnInit, ViewContainerRef } from "@angular/core";
 import { UntypedFormGroup } from "@angular/forms";
 import { FieldConfig } from "app/oc/interfaces";
-import { InputComponent } from "../dynamic-forms/components/input/input.component";
-import { ButtonComponent } from "../dynamic-forms/components/button/button.component";
-import { SelectComponent } from "../dynamic-forms/components/select/select.component";
-import { DateComponent } from "../dynamic-forms/components/date/date.component";
-import { RadiobuttonComponent } from "../dynamic-forms/components/radiobutton/radiobutton.component";
-import { CheckboxGroupComponent } from "../dynamic-forms/components/checkboxgroup/checkboxgroup.component";
-import { CheckboxComponent } from "../dynamic-forms/components/checkbox/checkbox.component";
-import { ComboboxComponent } from "../dynamic-forms/components/combobox/combobox.component";
-import { TextAreaComponent } from "../dynamic-forms/components/textarea/textarea.component";
-import { SubformComponent } from "../dynamic-forms/components/subform/subform.component";
-import { LabelComponent } from "../dynamic-forms/components/label/label.component";
-import { MenuComponent } from "../dynamic-forms/components/menu/menu.component";
-import { InvisibleComponent } from "../dynamic-forms/components/invisible/invisible.component";
-import { WidgetComponent } from "../dynamic-forms/components/widget/widget.component";
+import { FormsService } from "../services";
 
-const componentMapper = {
-    input: InputComponent,
-    button: ButtonComponent,
-    select: SelectComponent,
-    date: DateComponent,
-    radiobutton: RadiobuttonComponent,
-    checkboxgroup: CheckboxGroupComponent,
-    checkbox: CheckboxComponent,
-    menu: MenuComponent,
-    combobox: ComboboxComponent,
-    textarea: TextAreaComponent,
-    invisible: InvisibleComponent,
-    widget: WidgetComponent,
-    label: LabelComponent,
-    subform: SubformComponent,
-};
+
 @Directive({
     selector: "[dynamicField]",
 })
@@ -40,14 +12,9 @@ export class DynamicFieldDirective implements OnInit {
     @Input() group: UntypedFormGroup;
     @Input() readOnlyPage: boolean;
     componentRef: any;
-    constructor(private container: ViewContainerRef) {}
+    constructor(private container: ViewContainerRef, private formsService: FormsService) { }
     ngOnInit() {
-        // const factory = this.resolver.resolveComponentFactory(
-        //   componentMapper[this.field.type]
-        // );
-        this.componentRef = this.container.createComponent(
-            componentMapper[this.field.type],
-        );
+        this.componentRef = this.formsService.createComponent(this.container, this.field.type);
         this.componentRef.instance.field = this.field;
         this.componentRef.instance.group = this.group;
         this.componentRef.instance.readOnlyPage = this.readOnlyPage;
