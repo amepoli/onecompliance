0d0f2c54694aeea26fb3baaaf973e76cb3c3410e
amepoli <amedeo.poli@auditft.it>
2024-11-29 01:24:22 +0100

    problema risolto (o quasi)

--- sql/cancellazioni_profilazioni_finafarm.sql
+++ sql/cancellazioni_profilazioni_finafarm.sql
@@ -1,96 +1,67 @@
-WITH base_query AS (
-        SELECT  
-            snd.codice_azienda, 
-            coalesce(snd.data_esecuzione, snd.data_prevista) as data_esecuzione,
-            entrasp.scadenza_profilazione(ss.codice_azienda, ss.id_sondaggio, ss.id_somministrazione) AS data_scadenza,
-            ss.id_sondaggio, 
-            ss.id_somministrazione,
-            ss.object_key, 
-            ss.object_name,
-            entrasp.conta_sondaggi_successivi_entro_scadenza_incl_futuri(snd.codice_azienda, ss.id_sondaggio, ss.id_somministrazione) AS conta,
-            fn.*, -- Colonne della funzione laterale
-            CASE 
-                WHEN CURRENT_DATE < entrasp.scadenza_profilazione(ss.codice_azienda, ss.id_sondaggio, ss.id_somministrazione) THEN 'Non scaduto' 
-                ELSE 'Scaduto' 
-            END AS scad_non_scad
-        FROM 
-            entrasp.sondaggi_somministrati ss
-        INNER JOIN 
-            entrasp.sondaggi snd
-        ON 
-            ss.codice_azienda = snd.codice_azienda 
-            AND ss.id_sondaggio = snd.id_sondaggio
-        LEFT JOIN LATERAL 
-            entrasp.sondaggi_successivi_entro_scadenza_incl_futuri(
-                ss.codice_azienda, 
-                ss.id_sondaggio, 
-                ss.id_somministrazione
-            ) AS fn
-        ON TRUE
-        WHERE 
-            snd.codice_azienda = 'FINAFARM'
-            AND snd.id_modello_test = 526
-						and ss.object_key = 'FINAFARM|14691'
-    ),
-    max_conta_query AS (
+--select count(id_sondaggio) from entrasp.sondaggi where codice_azienda='FINAFARM' -35933
+ SET session_replication_role = replica;
+WITH sondaggi_con_scadenza_nel_futuro AS (
+   SELECT  
+    snd.codice_azienda, 
+    snd.id_sondaggio,
+	ss.id_somministrazione,
+    ss.object_key,
+    ss.object_description,
+    entrasp.scadenza_profilazione_estesa(ss.codice_azienda, ss.id_sondaggio, ss.id_somministrazione) AS data_scadenza,
+    ss.stato,
+    ROW_NUMBER() OVER (
+        PARTITION BY ss.object_key 
+        ORDER BY COALESCE(snd.data_esecuzione, snd.data_prevista)
+    ) AS rn
+FROM 
+    entrasp.sondaggi_somministrati ss
+INNER JOIN 
+    entrasp.sondaggi snd 
+USING 
+    (codice_azienda, id_sondaggio)
+WHERE 
+    snd.codice_azienda = 'FINAFARM' 
+ --   AND ss.object_key = 'FINAFARM|14691'
+    AND snd.id_modello_test = 526
+    AND entrasp.scadenza_profilazione_estesa(ss.codice_azienda, ss.id_sondaggio, ss.id_somministrazione) > CURRENT_DATE
+    and ss.stato != 'C'
+    AND snd.id_sondaggio NOT IN (
         SELECT 
-            object_key, 
-            object_name, 
-            MAX(conta) AS max_conta
+            pf.id_sondaggio 
         FROM 
-            base_query
-        WHERE 
-            conta > 1
-        GROUP BY 
-            object_key, 
-            object_name
-    ),
-    sondaggi_da_cancellare AS (
-        SELECT 
-            
-            bq.data_esecuzione,
-            bq.data_scadenza,
-            entrasp.scadenza_profilazione(bq.codice_azienda, bq.id_sondaggio, bq.id_somministrazione) AS dt_scad_orig,
-            bq.id_somministrazione, 
-            bq.object_key, 
-            bq.object_name,  
-            bq.*, -- Include le colonne derivate da fn nel base_query
-            CASE 
-                WHEN bq.prog = bq.max_prog THEN 'Ultimo' 
-                ELSE 'Non ultimo' 
-            END AS progressivo,
-            bq.scad_non_scad,
-            CASE 
-                WHEN bq.prog < bq.max_prog 
-                THEN 'Delete' 
-                ELSE 'Keep' 
-            END AS D_K
-        FROM 
-            base_query bq
-        INNER JOIN 
-            max_conta_query mcq
-        ON 
-            bq.object_key = mcq.object_key 
-            AND bq.conta = mcq.max_conta
-        WHERE 
-            bq.conta > 1
+            entrasp.progetti_fasi pf 
+        WHERE  
+            codice_azienda = 'FINAFARM'
+            AND pf.id_sondaggio IS NOT NULL
     )
-    -- Esegue la cancellazione per i sondaggi contrassegnati come 'Delete'
-    SELECT *   --entrasp.sondaggio_delete(sdc.codice_azienda, sdc.idsond)
-    FROM sondaggi_da_cancellare sdc 
-	
-    WHERE 
-		D_K = 'Delete'
-     -- AND sdc.idsond NOT IN (
-     --      SELECT sdc2.idsond
-     --      FROM sondaggi_da_cancellare sdc2 
-     --      WHERE sdc2.D_K = 'Keep'
-     --  )
-			-- And 
-			-- sdc.idsond not in (
-			-- 	select id_sondaggio from entrasp.progetti_fasi where codice_azienda='FINAFARM' and id_sondaggio is not null
-			-- )
-			And sdc.idsond not in (
-				select id_sondaggio from entrasp.risposte where codice_azienda='FINAFARM'
-			 )
-				order by idsond desc
+ORDER BY 
+    ss.object_key, 
+    COALESCE(snd.data_esecuzione, snd.data_prevista)
+    ), aggiorna_sondaggi as(
+		update entrasp.sondaggi snd
+		set id_sondaggio_succ=snf.id_sondaggio
+		from entrasp.sondaggi_somministrati ss, sondaggi_con_scadenza_nel_futuro snf
+		where snd.codice_azienda='FINAFARM'  
+		and snd.codice_azienda=ss.codice_azienda and snd.id_sondaggio=ss.id_sondaggio
+		and ss.object_key=snf.object_key
+		and snd.id_modello_test=526 
+		and snf.rn=1
+		and snd.id_sondaggio_succ in(select snf2.id_sondaggio from sondaggi_con_scadenza_nel_futuro snf2 where rn>1)
+	),
+	aggiorna_somministrazioni as(
+		update entrasp.sondaggi_somministrati ss
+		set id_sondaggio_succ=snf.id_sondaggio,
+		id_somministrazione_succ=snf.id_somministrazione
+		from entrasp.sondaggi snd, sondaggi_con_scadenza_nel_futuro snf
+		where ss.codice_azienda='FINAFARM' 
+		and snd.codice_azienda=ss.codice_azienda and snd.id_sondaggio=ss.id_sondaggio
+		and ss.object_key=snf.object_key
+		and snd.id_modello_test=526 
+		and snf.rn=1
+		and ss.id_somministrazione_succ in(select snf2.id_somministrazione from sondaggi_con_scadenza_nel_futuro snf2 where rn>1)
+	)
+       select entrasp.sondaggio_delete(codice_azienda, id_sondaggio)
+	   from sondaggi_con_scadenza_nel_futuro
+	   where rn>1
+	   order by object_key;
+	   SET session_replication_role = origin;
