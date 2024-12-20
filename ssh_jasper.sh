93030fce472666ec87f0bff8b2cbc705ee01fb29
amepoli <amedeo.poli@auditft.it>
2024-12-20 18:49:06 +0100

    file per collegamento a jasper

---
+++ ssh_jasper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i "/opt/keys/cdk-ec2-user.pem" -L 5432:occdkstackprod-2-auroraclusterfromsnapshotinstance-oko7ldaqyexf.caxbbckt9xen.eu-central-1.rds.amazonaws.com:5432 ec2-user@18.193.201.111
