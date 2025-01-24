303679fe59a577afc05db94d05340a89de31ccca
amepoli <amedeo.poli@auditft.it>
2025-01-24 17:44:50 +0100

    jasper mess

--- script_sh/scp_trasferimento_file_ec2_dev.sh
+++ script_sh/scp_trasferimento_file_ec2_dev.sh
@@ -1,2 +1,2 @@
-nome_file="ripristino_ultimo_backup_per_test.sh"
-scp -i "/opt/keys/cdkdev-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/$nome_file" ec2-user@3.124.34.69:/home/ec2-user/$nome_file
+nome_file="gorico.jar"
+scp -i "/opt/keys/cdkdev-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/$nome_file" ec2-user@3.124.34.69:/home/ec2-user/Jasper/$nome_file
