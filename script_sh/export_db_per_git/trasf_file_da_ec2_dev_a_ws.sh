bfee56fc924ad0547fd31cf4f0bf6daf708f3bf3
amepoli <amedeo.poli@auditft.it>
2025-02-07 20:02:48 +0100

    modifica dir destinazione

--- script_sh/export_db_per_git/trasf_file_da_ec2_dev_a_ws.sh
+++ script_sh/export_db_per_git/trasf_file_da_ec2_dev_a_ws.sh
@@ -1,1 +1,1 @@
-scp -i "/opt/keys/cdkdev-ec2-user.pem" -r ec2-user@3.124.34.69:/home/ec2-user/Database/entrasp/functions "/home/apoli/Development/Database/entrasp/functions"
+scp -i "/opt/keys/cdkdev-ec2-user.pem" -r ec2-user@3.124.34.69:/home/ec2-user/Database/entrasp/functions "/home/apoli/Development/Database/entrasp"
