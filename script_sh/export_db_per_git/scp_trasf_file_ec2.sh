b5b0d06b5793f93b4071b4279c7c7bb299a24332
amepoli <amedeo.poli@auditft.it>
2025-02-08 14:47:02 +0100

    export tables, functions, trigger_functions

--- script_sh/export_db_per_git/scp_trasf_file_ec2.sh
+++ script_sh/export_db_per_git/scp_trasf_file_ec2.sh
@@ -1,3 +1,7 @@
-nome_file="export_functions.sh"
+nome_file="export_functions_v2.sh"
+nome_file2="export_trigger_functions_v2.sh"
+nome_file3="export_tables_v2.sh"
 
 scp -i "/opt/keys/cdk-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/export_db_per_git/$nome_file" ec2-user@18.193.201.111:/home/ec2-user/Database/$nome_file
+scp -i "/opt/keys/cdk-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/export_db_per_git/$nome_file2" ec2-user@18.193.201.111:/home/ec2-user/Database/$nome_file2
+scp -i "/opt/keys/cdk-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/export_db_per_git/$nome_file3" ec2-user@18.193.201.111:/home/ec2-user/Database/$nome_file3
--- script_sh/export_db_per_git/trasf_file_da_ec2_a_ws.sh
+++ script_sh/export_db_per_git/trasf_file_da_ec2_a_ws.sh
@@ -1,1 +1,3 @@
 scp -i "/opt/keys/cdk-ec2-user.pem" -r ec2-user@18.193.201.111:/home/ec2-user/Database/entrasp/functions "/home/apoli/Development/Database/entrasp"
+scp -i "/opt/keys/cdk-ec2-user.pem" -r ec2-user@18.193.201.111:/home/ec2-user/Database/entrasp/trigger_functions "/home/apoli/Development/Database/entrasp"
+scp -i "/opt/keys/cdk-ec2-user.pem" -r ec2-user@18.193.201.111:/home/ec2-user/Database/entrasp/tables "/home/apoli/Development/Database/entrasp"
