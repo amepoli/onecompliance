d670724810f24f86c14de5d2e51876e8eb770bf4
amepoli <amedeo.poli@auditft.it>
2025-02-08 14:56:04 +0100

    rationalization of scripts

--- script_sh/scp_trasferimento_file_ec2.sh
+++
@@ -1,3 +0,0 @@
-nome_file="export_functions.sh"
-
-scp -i "/opt/keys/cdk-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/$nome_file" ec2-user@18.193.201.111:/home/ec2-user/$nome_file
--- script_sh/scp_trasferimento_file_ec2_dev.sh
+++
@@ -1,4 +0,0 @@
-nome_file="export_functions_v2.sh"
-nome_file2="export_trigger_functions_v2.sh"
-scp -i "/opt/keys/cdkdev-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/export_db_per_git/$nome_file" ec2-user@3.124.34.69:/home/ec2-user/Database/$nome_file
-scp -i "/opt/keys/cdkdev-ec2-user.pem" "/home/apoli/Development/onecompliance/script_sh/export_db_per_git/$nome_file" ec2-user@3.124.34.69:/home/ec2-user/Database/$nome_file2
