f7327bb13e81971b2ffe5f73df0cd6d8ccbb8cdd
amepoli <amedeo.poli@auditft.it>
2025-03-31 18:42:38 +0200

    changed destination directory of backup

--- script_sh/Gestione_backup/trasferisci_in_locale_dbbackup.sh
+++ script_sh/Gestione_backup/trasferisci_in_locale_dbbackup.sh
@@ -1,4 +1,4 @@
 nome_file="gorico_prod-1_last.backup"
 
 # Trasferisco il file di backup dal server remoto al server locale
-scp -i /opt/keys/cdk-ec2-user.pem ec2-user@18.193.201.111:/home/ec2-user/sftp/customers/backups/$nome_file /home/Downloads/$nome_file
+scp -i /opt/keys/cdk-ec2-user.pem ec2-user@18.193.201.111:/home/ec2-user/sftp/customers/backups/$nome_file /home/apoli/Downloads/$nome_file
